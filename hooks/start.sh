60cf9c77d65620a3fe9958f4711953cfff6ab2dc
Alexander V. Pavlovcik <alexander@pavlovcik.com>
2021-05-29 12:31:38 -0500

    sweet spinner

--- hooks/start.sh
+++ hooks/start.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-if [ -f ./contracts/.env ]
-then
+# SPINNER FUNCTIONS
+
+sp="⠁⠂⠄⡀⢀⠠⠐⠈"
+sc=0
+spin() {
+  printf "\b${sp:sc++:1}"
+  ((sc == ${#sp})) && sc=0
+}
+endspin() {
+  printf "\r%s\n" "$@"
+}
+
+if [ -f ./contracts/.env ]; then
   source ./contracts/.env
 else
   echo "Please add a .env inside the contracts folder."
@@ -12,13 +23,16 @@ cd ./contracts || echo "ERROR: ./contracts/ doesn't exist?"
 yarn install
 yarn compile
 kill $(lsof -t -i:8545) || true
-yarn hardhat node --fork https://eth-mainnet.alchemyapi.io/v2/$ALCHEMY_API_KEY --fork-block-number 12150000 --show-accounts --export-all tmp-uad-contracts-deployment.json > ../local.node.log 2>&1 &
+yarn hardhat node --fork https://eth-mainnet.alchemyapi.io/v2/$ALCHEMY_API_KEY --fork-block-number 12150000 --show-accounts --export-all tmp-uad-contracts-deployment.json >../local.node.log 2>&1 &
 sleep 10
-while : ; do
-    [[ -f "tmp-uad-contracts-deployment.json" ]] && break
-    echo "Pausing until uad-contracts-deployment.json exists."
-    sleep 5
+echo "Pausing until uad-contracts-deployment.json exists."
+while :; do
+  spin
+  [[ -f "tmp-uad-contracts-deployment.json" ]] && break
+  sleep .06
 done
+endspin
+
 node ../hooks/process-deployment.js ./tmp-uad-contracts-deployment.json ../frontend/src/uad-contracts-deployment.json
 rm -f ./tmp-uad-contracts-deployment.json
 
