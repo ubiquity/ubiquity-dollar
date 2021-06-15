acb6511aa026edd3dded1112fa66a444bb44032c
アレクサンダー U B I Q U I T Y <gpg@pavlovcik.com>
2021-06-15 16:13:38 -0500

    build

--- hooks/next-build.sh
+++ hooks/next-build.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
 cd contracts/
-npm install
-yes | npx hardhat compile
+echo "--- 1 ---"
+sleep 1
+yarn
+echo "--- 2 ---"
+sleep 1
+yes | yarn hardhat compile
+echo "--- 3 ---"
+sleep 1
 mkdir -p frontend/src/types
+echo "--- 4 ---"
+sleep 1
 cp -r contracts/artifacts/types frontend/src/types
+echo "--- 5 ---"
+sleep 1
 cd ../frontend/
-npm install
-npx next build
-npx next start
+echo "--- 6 ---"
+sleep 1
+yarn
+echo "--- 7 ---"
+sleep 1
+yarn next build
+echo "--- 8 ---"
+sleep 1
+yarn next start
+echo "--- 9 ---"
+sleep 1
