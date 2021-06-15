63364e93cfdd87fce9631a332d725f26df103f73
アレクサンダー U B I Q U I T Y <gpg@pavlovcik.com>
2021-06-15 18:53:12 -0500

    build

--- hooks/next-build.sh
+++ hooks/next-build.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-
-
 cd contracts/
 echo "--- 0 ---"
 yarn
 echo "--- 1 ---"
-yarn add hardhat
+# yarn add hardhat
 echo "--- 2 ---"
-TS_NODE_TRANSPILE_ONLY=1 npx hardhat compile
+TS_NODE_TRANSPILE_ONLY=1 yarn hardhat compile
 echo "--- 3 ---"
 mkdir -p frontend/src/types
 echo "--- 4 ---"
