1f3df1b02e2061e40d0d87f116f79da7844ea6c1
アレクサンダー U B I Q U I T Y <gpg@pavlovcik.com>
2021-06-15 19:23:27 -0500

    wow

--- hooks/next-build.sh
+++ hooks/next-build.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 cd contracts/
-echo "--- 0 ---"
+echo "✅ --- 0 ---"
 yarn
-echo "--- 1 ---"
+echo "✅ --- 1 ---"
 yarn add hardhat
-echo "--- 2 ---"
+echo "✅ --- 2 ---"
 export TS_NODE_TRANSPILE_ONLY=1 && yarn hardhat compile
-echo "--- 3 ---"
-mkdir -p frontend/src/types
-echo "--- 4 ---"
-cp -r contracts/artifacts/types frontend/src/types
-echo "--- 5 ---"
+echo "✅ --- 3 ---"
+# mkdir -p ../frontend/src/
+echo "✅ --- 4 ---"
+cp -r artifacts/types ../frontend/src/
+echo "✅ --- 5 ---"
 cd ../frontend/
-echo "--- 6 ---"
+echo "✅ --- 6 ---"
 yarn
-echo "--- 7 ---"
+echo "✅ --- 7 ---"
 yarn next build
 echo "--- 8 ---"
 yarn next start
