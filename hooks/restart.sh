77aecf4b53c593b17ea3df90513c62b44727ec70
アレクサンダー U B I Q U I T Y <gpg@pavlovcik.com>
2021-06-15 21:52:05 -0500

    build

--- hooks/restart.sh
+++ hooks/restart.sh
@@ -3,6 +3,7 @@
 git add .
 git commit -am "build"
 # git push --set-upstream origin feature/build-vercel
+git push
 cd ~/Downloads
 rm -rf ./uad-ui-launch-testenv/
 git clone https://github.com/pavlovcik/uad-ui-launch.git uad-ui-launch-testenv/
