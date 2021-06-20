932f9ed2eefe7f324ae23f5ae3712000728b25fe
アレクサンダー U B I Q U I T Y <gpg@pavlovcik.com>
2021-06-19 23:36:29 -0500

    font and dark colors

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import "./styles/index.css";
+import './styles/proxima.css'
 
 import { ConnectedNetwork } from "../components/context/connected";
 type MyComponentProps = React.PropsWithChildren<{
