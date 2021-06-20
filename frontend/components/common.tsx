0367bdc410a1fdc5f51a28d814784e834acdb6b4
アレクサンダー U B I Q U I T Y <gpg@pavlovcik.com>
2021-06-20 06:56:18 -0500

    general style updates

--- frontend/components/common.tsx
+++ frontend/components/common.tsx
@@ -216,12 +216,12 @@ export function _renderControls() {
               <span>uAD</span>
             </div>
             <div>
-              <a href="https://crv.to/pool">
+              <a target="_blank" href="https://crv.to/pool">
                 <input type="button" value="Deposit" />
               </a>
             </div>
             <div>
-              <a href="https://crv.to">
+              <a target="_blank" href="https://crv.to">
                 <input type="button" value="Swap" />
               </a>
             </div>
@@ -239,12 +239,12 @@ export function _renderControls() {
               <span>UBQ</span>
             </div>
             <div>
-              <a href="https://app.sushi.com/add/0x4e38D89362f7e5db0096CE44ebD021c3962aA9a0/0x0F644658510c95CB46955e55D7BA9DDa9E9fBEc6">
+              <a target="_blank" href="https://app.sushi.com/add/0x4e38D89362f7e5db0096CE44ebD021c3962aA9a0/0x0F644658510c95CB46955e55D7BA9DDa9E9fBEc6">
                 <input type="button" value="Deposit" />
               </a>
             </div>
             <div>
-              <a href="https://app.sushi.com/swap?inputCurrency=0x4e38D89362f7e5db0096CE44ebD021c3962aA9a0&outputCurrency=0x0F644658510c95CB46955e55D7BA9DDa9E9fBEc6">
+              <a target="_blank" href="https://app.sushi.com/swap?inputCurrency=0x4e38D89362f7e5db0096CE44ebD021c3962aA9a0&outputCurrency=0x0F644658510c95CB46955e55D7BA9DDa9E9fBEc6">
                 <input type="button" value="Swap" />
               </a>
             </div>
