2462ca97b589d8f817312d3f086ffe193d6f0487
Benjamin <benjamin.mateo@protonmail.com>
2021-07-31 10:35:40 +0200

    fix(ubq) pending ubq

--- frontend/components/chefugov.tsx
+++ frontend/components/chefugov.tsx
@@ -2,7 +2,9 @@ import { ethers } from "ethers";
 import { Balances, useConnectedContext } from "./context/connected";
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import {
-  MasterChef__factory,
+  BondingShareV2,
+  BondingShareV2__factory,
+  MasterChefV2__factory,
   UbiquityAlgorithmicDollarManager,
   UbiquityGovernance__factory,
 } from "../src/types";
@@ -38,11 +40,19 @@ async function _getUBQReward(
   const SIGNER = provider?.getSigner();
 
   if (SIGNER && manager && account) {
-    const masterChef = MasterChef__factory.connect(
+    const masterChef = MasterChefV2__factory.connect(
       await manager.masterChefAddress(),
       SIGNER
     );
-    const balance = await masterChef?.pendingUGOV(account);
+    const bondingShareAdr = await manager.bondingShareAddress();
+    const bondingShare = BondingShareV2__factory.connect(
+      bondingShareAdr,
+      SIGNER
+    );
+    const bondingShareIds = await bondingShare.holderTokens(account);
+    // TODO there can be several Ids so we should be able to select one
+
+    const balance = await masterChef?.pendingUGOV(bondingShareIds[0]);
     if (balance) {
       if (!(balance.toString() === reward)) {
         setRewards(ethers.utils.formatEther(balance));
@@ -63,12 +73,18 @@ async function _claimReward(
   const SIGNER = provider?.getSigner();
 
   if (SIGNER && manager && account) {
-    const masterChef = MasterChef__factory.connect(
+    const masterChef = MasterChefV2__factory.connect(
       await manager.masterChefAddress(),
       SIGNER
     );
-
-    await (await masterChef?.getRewards()).wait();
+    const bondingShareAdr = await manager.bondingShareAddress();
+    const bondingShare = BondingShareV2__factory.connect(
+      bondingShareAdr,
+      SIGNER
+    );
+    const bondingShareIds = await bondingShare.holderTokens(account);
+    // TODO there can be several Ids so we should be able to select one
+    await (await masterChef?.getRewards(bondingShareIds[0])).wait();
     await _getUBQReward(account, manager, provider, rewards, setRewards);
     await _getUBQBalance(account, manager, provider, balances, setBalances);
   }
@@ -112,7 +128,7 @@ const ChefUgov = () => {
     );
   };
   const handleReward = async () => {
-    console.log(`handling reward`)
+    console.log(`handling reward`);
     _getUBQReward(
       account ? account.address : "",
       manager,
@@ -138,16 +154,24 @@ const ChefUgov = () => {
     );
   };
 
-setInterval(handleReward, 8e4)
+  setInterval(handleReward, 8e4);
 
   return (
     <>
       <div id="chefugov">
         {/* <button onClick={handleReward}>Get Ubiquity Rewards</button> */}
-        <p><span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 91.57 104.19"><path d="M43.28.67 2.5 24.22A5 5 0 0 0 0 28.55v47.09A5 5 0 0 0 2.5 80l40.78 23.55a5 5 0 0 0 5 0L89.07 80a5 5 0 0 0 2.5-4.33V28.55a5 5 0 0 0-2.5-4.33L48.28.67a5 5 0 0 0-5 0zm36.31 25a2 2 0 0 1 0 3.46l-6 3.48c-2.72 1.57-4.11 4.09-5.34 6.3-.18.33-.36.66-.55 1-3 5.24-4.4 10.74-5.64 15.6C59.71 64.76 58 70.1 50.19 72.09a17.76 17.76 0 0 1-8.81 0c-7.81-2-9.53-7.33-11.89-16.59-1.24-4.86-2.64-10.36-5.65-15.6l-.54-1c-1.23-2.21-2.62-4.73-5.34-6.3l-6-3.47a2 2 0 0 1 0-3.47L43.28 7.6a5 5 0 0 1 5 0zM43.28 96.59 8.5 76.51A5 5 0 0 1 6 72.18v-36.1a2 2 0 0 1 3-1.73l6 3.46c1.29.74 2.13 2.25 3.09 4l.6 1c2.59 4.54 3.84 9.41 5 14.11 2.25 8.84 4.58 18 16.25 20.93a23.85 23.85 0 0 0 11.71 0C63.3 75 65.63 65.82 67.89 57c1.2-4.7 2.44-9.57 5-14.1l.59-1.06c1-1.76 1.81-3.27 3.1-4l5.94-3.45a2 2 0 0 1 3 1.73v36.1a5 5 0 0 1-2.5 4.33L48.28 96.59a5 5 0 0 1-5 0z"  /></svg></span><span>{rewards}</span></p>
-        <button id="claimer" onClick={handleClaim}><span>Claim Pending Ubiquity Rewards</span></button>
+        <p>
+          <span>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 91.57 104.19">
+              <path d="M43.28.67 2.5 24.22A5 5 0 0 0 0 28.55v47.09A5 5 0 0 0 2.5 80l40.78 23.55a5 5 0 0 0 5 0L89.07 80a5 5 0 0 0 2.5-4.33V28.55a5 5 0 0 0-2.5-4.33L48.28.67a5 5 0 0 0-5 0zm36.31 25a2 2 0 0 1 0 3.46l-6 3.48c-2.72 1.57-4.11 4.09-5.34 6.3-.18.33-.36.66-.55 1-3 5.24-4.4 10.74-5.64 15.6C59.71 64.76 58 70.1 50.19 72.09a17.76 17.76 0 0 1-8.81 0c-7.81-2-9.53-7.33-11.89-16.59-1.24-4.86-2.64-10.36-5.65-15.6l-.54-1c-1.23-2.21-2.62-4.73-5.34-6.3l-6-3.47a2 2 0 0 1 0-3.47L43.28 7.6a5 5 0 0 1 5 0zM43.28 96.59 8.5 76.51A5 5 0 0 1 6 72.18v-36.1a2 2 0 0 1 3-1.73l6 3.46c1.29.74 2.13 2.25 3.09 4l.6 1c2.59 4.54 3.84 9.41 5 14.11 2.25 8.84 4.58 18 16.25 20.93a23.85 23.85 0 0 0 11.71 0C63.3 75 65.63 65.82 67.89 57c1.2-4.7 2.44-9.57 5-14.1l.59-1.06c1-1.76 1.81-3.27 3.1-4l5.94-3.45a2 2 0 0 1 3 1.73v36.1a5 5 0 0 1-2.5 4.33L48.28 96.59a5 5 0 0 1-5 0z" />
+            </svg>
+          </span>
+          <span>{rewards}</span>
+        </p>
+        <button id="claimer" onClick={handleClaim}>
+          <span>Claim Pending Ubiquity Rewards</span>
+        </button>
       </div>
-
     </>
   );
 };
