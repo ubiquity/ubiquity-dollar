49b9722daf8c190cef11f5439a46fdd86e2d8972
Zequez <zequez@gmail.com>
2021-09-02 19:59:41 -0300

    Add Tailwind with explicit style output

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,4 @@
-const WindiCSS = require("windicss-webpack-plugin").default;
-
 module.exports = {
-  webpack: (config) => {
-    config.plugins.push(new WindiCSS());
-    return config;
-  },
   future: {
     webpack5: true,
   },
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import "./styles/index.css";
 import "./styles/proxima.css";
-import "windi.css";
+import "./styles/tailwind.css";
 
 import { ConnectedNetwork } from "../components/context/connected";
 import { AppProps } from "next/app";
