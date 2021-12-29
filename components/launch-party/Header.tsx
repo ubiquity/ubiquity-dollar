559c900535119d8977c9c135ba7d8a38b5e0a49f
Zequez <zequez@gmail.com>
2021-12-29 11:11:11 -0300

    Big header

---
+++ components/launch-party/Header.tsx
@@ -0,0 +1,15 @@
+import { Icon } from "../ui/icons";
+
+const Header = () => {
+  return (
+    <header className="p-4 mb-6">
+      <div>{<Icon icon="uad" className="align-middle h-24 mb-4 fill-accent drop-shadow-light" />}</div>
+      <a href="/" className="group text-6xl tracking-wider font-thin uppercase text-gray-400 hover:text-accent no-underline hover:drop-shadow-light">
+        <span>Ubiquity</span>
+      </a>
+      <div className="text-3xl font-thin text-white text-opacity-75">Launch Party</div>
+    </header>
+  );
+};
+
+export default Header;
