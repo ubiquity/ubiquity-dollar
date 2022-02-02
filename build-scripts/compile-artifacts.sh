cc8fb31664e834c587595af85ff0aeb899f773a7
Zequez <zequez@gmail.com>
2022-02-02 19:04:56 -0300

    Normalized build tasks for launch party

--- build-scripts/compile-artifacts.sh
+++ build-scripts/compile-artifacts.sh
@@ -11,13 +11,3 @@ yarn build
 rm -f $UP$DEPLOYMENT_ARTIFACT
 yarn hardhat export --export $UP$DEPLOYMENT_ARTIFACT --network mainnet
 cd $UP || exit 1
-
-cd ./the-ubiquity-stick || echo "ERROR: ./the-ubiquity-stick/ doesn't exist?"
-yarn install
-yarn build
-
-cd $UP || exit 1
-
-yarn typechain
-
-exit 0
