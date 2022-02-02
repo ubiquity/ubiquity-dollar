87e6ff4e2bc258ff21933010698114fd7d9ef999
Zequez <zequez@gmail.com>
2022-02-02 19:48:32 -0300

    Remove artifacts that now are loaded from submodule

--- components/launch-party/lib/types/index.ts
+++
@@ -1,6 +0,0 @@
-export type { TheUbiquityStick } from "./TheUbiquityStick";
-export type { TheUbiquityStickSale } from "./TheUbiquityStickSale";
-export type { SimpleBond } from "./SimpleBond";
-export { TheUbiquityStick__factory } from "./factories/TheUbiquityStick__factory";
-export { TheUbiquityStickSale__factory } from "./factories/TheUbiquityStickSale__factory";
-export { SimpleBond__factory } from "./factories/SimpleBond__factory";
