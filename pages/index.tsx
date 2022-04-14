2cc51997021ce491e23baafbe923715083d6034a
Zequez <zequez@gmail.com>
2022-04-13 23:49:00 -0300

    Uncomment something that broke the debt coupon component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,12 @@ import PriceStabilization from "./price-stabilization";
 export default PriceStabilization;
 // import { FC } from "react";
 // import { _renderControls } from "../components/common";
-// import FullDeployment from "../fixtures/ubiquity-dollar-deployment.json";
+import FullDeployment from "../fixtures/ubiquity-dollar-deployment.json";
 
-// export const ADDRESS = {
-//   MANAGER: FullDeployment.contracts.UbiquityAlgorithmicDollarManager.address,
-//   DEBT_COUPON_MANAGER: FullDeployment.contracts.DebtCouponManager.address,
-// };
+export const ADDRESS = {
+  MANAGER: FullDeployment.contracts.UbiquityAlgorithmicDollarManager.address,
+  DEBT_COUPON_MANAGER: FullDeployment.contracts.DebtCouponManager.address,
+};
 
 // const Index: FC = (): JSX.Element => {
 //   // const [provider, setProvider] = useState<ethers.providers.Web3Provider>();
