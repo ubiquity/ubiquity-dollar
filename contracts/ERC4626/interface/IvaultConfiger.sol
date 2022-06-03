82c32169e8cfda7a93f0da801f14651b16b4f15e
seosigoto <seo.sigoto@gmail.com>
2022-06-03 13:48:36 +0900

    configur function

--- contracts/ERC4626/interface/IvaultConfiger.sol
+++ contracts/ERC4626/interface/IvaultConfiger.sol
@@ -2,11 +2,19 @@
 pragma solidity >=0.8.0;
 
 interface IVaultConfigurator {
+    event WhitelistAdded(address receiver);
+    event WhitelistRemoved(address receiver);
+    event StrategyAllowed(address receiver);
+    event StrategyPrevented(address receiver);
+    event Puased();
+    event UnPuased();
+
     function addWhitelist(address receiver) external;
     function removeWhitelist(address receiver) external;
     function allowStrategy(address strategyAddr) external;
     function prevetnStrategy(address strategyAddr) external;
     function pause() external;
     function unpause() external;
-    
+    function updateuCRrate(uint256) external;
+    function twapPriceProvde(address )external view returns(uint256);
 }
