be2bcfd36a58c6b39587ba7415727381ab0135bc
osboxes.org <codercrane@gmail.com>
2022-07-19 13:34:28 -0400

    localchain

---
+++ web/dapp/_next/static/chunks/pages/index-703bf1eb91f1e820e202.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{2562:function(n,u,t){"use strict";t.r(u),t.d(u,{default:function(){return _.default}});var _=t(7112)},5301:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return t(2562)}])}},function(n){n.O(0,[112,774,888,179],(function(){return u=5301,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
