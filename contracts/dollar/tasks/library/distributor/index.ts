8af826a5a19e9d0ca5fc78872f1f09242e1da7bf
アレクサンダー.eth <gpg@pavlovcik.com>
2022-07-26 05:42:06 +0800

    refactor(distributor): convenience function to console log in disperse app friendly format

--- contracts/dollar/tasks/library/distributor/index.ts
+++ contracts/dollar/tasks/library/distributor/index.ts
@@ -4,6 +4,7 @@ import { getInvestors } from "./distributor-library/getInvestors";
 import transferFilter from "./distributor-library/log-filters/transfers";
 import { Tranche, transfersToInvestorsFilterWrapper } from "./distributor-library/log-filters/transfers-to-investors";
 import { readContractTransactionHistory } from "./distributor-library/read-contract-transaction-history";
+import { InvestorWithTransfers } from "./distributor-library/investor-types";
 const vestingRange = ["2022-05-01T00:00:00.000Z", "2024-05-01T00:00:00.000Z"];
 
 /**
@@ -20,20 +21,34 @@ interface TaskArgs {
 }
 
 export async function _distributor(taskArgs: TaskArgs, hre: HardhatRuntimeEnvironment) {
+  console.log(" getting investors...");
   const investors = await getInvestors(taskArgs.investors); // 1
-  console.log({ investors });
+  console.log(investors);
 
+  console.log(" getting transactionHistories...");
   const transactionHistories = await readContractTransactionHistory(taskArgs.token, vestingRange);
-  console.log({ transactionHistories });
+  console.log(transactionHistories);
 
   const transfersToAnybody = transactionHistories.filter(transferFilter);
   const transfersToInvestorsFilter = transfersToInvestorsFilterWrapper(investors);
 
+  console.log(" getting tranches...");
   const tranches = transfersToAnybody.map(transfersToInvestorsFilter).filter(Boolean) as Tranche[];
-  console.log({ tranches });
+  console.log(tranches);
 
+  console.log(" getting owed...");
   const owed = await calculateOwedUbqEmissions(investors, tranches, hre);
-  console.log({ owed });
+  console.log(owed);
+
+  console.log(displayInDisperseAppFriendlyFormat(owed).join("\n"));
 
   return owed;
+}
+
+type Owed = ({
+  owed: number;
+} & InvestorWithTransfers)[];
+
+function displayInDisperseAppFriendlyFormat(owed: Owed) {
+  return owed.map((owe) => [owe.address, owe.owed].join(", "));
 }
