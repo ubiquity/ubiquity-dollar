88e6b71aa4f31e478093ffd06f37a51b320fbb6c
george-shammer <george.gbenle@outlook.com>
2022-07-29 23:34:27 +0100

    Fix compile error of Identifier already declared.

--- contracts/dollar/src/UbiquityAlgorithmicDollarManager.sol
+++ contracts/dollar/src/UbiquityAlgorithmicDollarManager.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.3;
 import "openzeppelin-access/AccessControl.sol";
 import "openzeppelin-token-ERC20/ERC20.sol";
 import "openzeppelin-token-ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "openzeppelin-token-ERC20/utils/SafeERC20.sol";
 
 import "./interfaces/IUbiquityAlgorithmicDollar.sol";
 import "./interfaces/ICurveFactory.sol";
