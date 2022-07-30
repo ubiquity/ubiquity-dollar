09fdd608467ba1a2d8dcecbd87ecc0eda44a3208
george-shammer <george.gbenle@outlook.com>
2022-07-30 16:04:41 +0100

    Import dependencies for LP.sol

--- contracts/ubiquistick/src/LP.sol
+++ contracts/ubiquistick/src/LP.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.3;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "openzeppelin-token-ERC20/ERC20.sol";
+import "openzeppelin-access/Ownable.sol";
 
 contract LP is ERC20, Ownable {
   constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
