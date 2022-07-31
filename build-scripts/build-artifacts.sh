87cb81db8c6b2680b8da799cd73bdaec59ea97e0
アレクサンダー.eth <gpg@pavlovcik.com>
2022-07-31 10:57:27 +0800

    refactor(packagejson): simplification

---
+++ build-scripts/build-artifacts.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+cd "$(npm root)" || exit 1
+cd "$1" || exit 1
+
+echo "BUILDING ARTIFACTS"
+
+yarn hardhat export --network mainnet
+yarn hardhat export --network hardhat
+
+# cd "$(npm root)" || exit 1
+# cd ..
+# cd ./contracts/dollar || echo "ERROR: ./contracts/dollar/ doesn't exist?"
+
+# UP=../
+# DEPLOYMENT_ARTIFACT=fixtures/ubiquity-dollar-deployment.json
+
+# yarn install
+# yarn build
+
+# rm -f $UP$DEPLOYMENT_ARTIFACT
+# yarn hardhat export --export $UP$DEPLOYMENT_ARTIFACT --network mainnet &
+# yarn hardhat export --export $UP$DEPLOYMENT_ARTIFACT --network hardhat
+# cd $UP || exit 1
