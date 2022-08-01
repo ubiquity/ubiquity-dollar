6bc15d281bb7c83c84cd4609fba6309ff0b7ea23
アレクサンダー.eth <gpg@pavlovcik.com>
2022-08-02 05:47:13 +0800

    remove(tailwind): starting from scratch with vanilla CSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -43,8 +43,6 @@ export default function Ubiquity({ Component, pageProps }: AppProps): JSX.Elemen
         <link rel="icon" type="image/png" sizes="192x192" href="https://dao.ubq.fi/favicon-192x192.png" />
         <link rel="icon" type="image/png" sizes="310x310" href="https://dao.ubq.fi/favicon-310x310.png" />
         <link rel="icon" type="image/png" sizes="512x512" href="https://dao.ubq.fi/favicon-512x512.png" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" />
         <meta name="theme-color" content="#06061a" />
         {process.env.NODE_ENV !== "production" && <script dangerouslySetInnerHTML={{ __html: noOverlayWorkaroundScript }} />}
       </Head>
@@ -53,7 +51,6 @@ export default function Ubiquity({ Component, pageProps }: AppProps): JSX.Elemen
           <Component {...pageProps} />
         </Layout>
       </AppContextProvider>
-      <script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"></script>
     </>
   );
 }
