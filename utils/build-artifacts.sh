24a3b6ba49b726904bc7addd19147257c85f93a3
0xlmao <110471113+0xlmao@users.noreply.github.com>
2022-08-04 11:47:27 +0800

    Update build-artifacts.sh

--- utils/build-artifacts.sh
+++ utils/build-artifacts.sh
@@ -7,17 +7,3 @@ cd "$1" || exit 1
 yarn hardhat export --network mainnet
 yarn hardhat export --network hardhat
 
-# cd "$(npm root)" || exit 1
-# cd ..
-# cd ./contracts/dollar || echo "ERROR: ./contracts/dollar/ doesn't exist?"
-
-# UP=../
-# DEPLOYMENT_ARTIFACT=fixtures/ubiquity-dollar-deployment.json
-
-# yarn install
-# yarn build
-
-# rm -f $UP$DEPLOYMENT_ARTIFACT
-# yarn hardhat export --export $UP$DEPLOYMENT_ARTIFACT --network mainnet &
-# yarn hardhat export --export $UP$DEPLOYMENT_ARTIFACT --network hardhat
-# cd $UP || exit 1
