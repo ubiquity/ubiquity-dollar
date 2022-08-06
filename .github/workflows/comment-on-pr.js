02557e8a741dc88624defd85d31e463989d3e3ec
アレクサンダー.eth <gpg@pavlovcik.com>
2022-08-06 10:39:35 +0800

    test(ci)

--- .github/workflows/comment-on-pr.js
+++ .github/workflows/comment-on-pr.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 export default async function commentOnPr(github, context) {
   const issue_number = Number(fs.readFileSync("./pr_number"));
   const deployments_log = fs.readFileSync("./deployments.log");
-  await github.rest.issues.createComment({
+  return await github.rest.issues.createComment({
     owner: context.repo.owner,
     repo: context.repo.repo,
     issue_number: issue_number,
