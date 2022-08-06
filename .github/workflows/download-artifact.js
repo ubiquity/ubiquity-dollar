4f6e97120896563d8dad0cd4b5bbc53f88e4ea9a
アレクサンダー.eth <gpg@pavlovcik.com>
2022-08-06 17:54:44 +0800

    test(ci)

--- .github/workflows/download-artifact.js
+++ .github/workflows/download-artifact.js
@@ -4,7 +4,7 @@ export default async function downloadArtifact(github, context, RUN_ID) {
     repo: context.repo.repo,
     run_id: RUN_ID,
   });
-  const matchArtifact = artifacts.data.artifacts.filter((artifact) => artifact.name == "pr")[0]
+  const matchArtifact = artifacts.data.artifacts.filter((artifact) => artifact.name == "pr")[0];
   const download = await github.actions.downloadArtifact({
     owner: context.repo.owner,
     repo: context.repo.repo,
