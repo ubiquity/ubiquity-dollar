323c4c67fbcea061892137877f0e17e405877071
george-shammer <george.gbenle@outlook.com>
2022-08-07 12:58:33 +0100

    Add deploy script for UARForDollarsCalculator.sol

---
+++ contracts/dollar/script/UARForDollarsCalculator.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import "../src/UbiquityAlgorithmicDollarManager.sol";
+import "../src/UARForDollarsCalculator.sol";
+
+contract UARForDollarsCalculatorScript is Script {
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+        UbiquityAlgorithmicDollarManager ubiquityAlgorithmicDollarManager = new UbiquityAlgorithmicDollarManager(
+                address(0x1)
+            );
+        address manager = address(ubiquityAlgorithmicDollarManager);
+
+        new UARForDollarsCalculator(manager);
+        vm.stopBroadcast();
+    }
+}
