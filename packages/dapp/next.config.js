6fc16adb39d66c3e7dbdc10274b0077410b3feb2
fantasy <angrybird02211@gmail.com>
2022-09-15 02:55:26 +0900

    rewrote infura id

--- packages/dapp/next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  publicRuntimeConfig: {
-    API_KEY_ALCHEMY: process.env.API_KEY_ALCHEMY,
-    API_KEY_COINMARKETCAP: process.env.API_KEY_COINMARKETCAP,
-    API_KEY_ETHERSCAN: process.env.API_KEY_ETHERSCAN,
-    MNEMONIC: process.env.MNEMONIC,
-  },
-};
