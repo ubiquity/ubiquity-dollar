f0e0e7444a320e2be9fe5d9509c38a8c6a8309d7
0xcodercrane <codercrane@gmail.com>
2022-10-08 01:46:00 +0800

    delete wrong tests

--- packages/contracts/test/dollar/ERC1155Ubiquity.t.sol
+++ packages/contracts/test/dollar/ERC1155Ubiquity.t.sol
@@ -20,75 +20,7 @@ contract ERC1155UbiquityTest is LocalTestHelper {
         token_addr = address(new ERC1155Ubiquity(uad_manager_addr, "https://ipfs.io/ipfs/mock"));
     }
 
-    function test_setSymbol() public {
-        vm.expectRevert("ERC20: deployer must be manager admin");
-        ERC20Ubiquity(token_addr).setSymbol("Test1");
-
-        vm.prank(admin);
-        ERC20Ubiquity(token_addr).setSymbol("Test1");
-        assertEq(ERC20Ubiquity(token_addr).symbol(), "Test1");
-    }
-
-    function test_setName() public {
-        vm.expectRevert("ERC20: deployer must be manager admin");
-        ERC20Ubiquity(token_addr).setName("Test1");
-
-        vm.prank(admin);
-        ERC20Ubiquity(token_addr).setName("Test1");
-        assertEq(ERC20Ubiquity(token_addr).name(), "Test1");
-    }
-
-    function test_mintAndBurn() public {
-
-        // test onlyMinter and mint
-        address mock_addr1 = address(0x123);
-        address mock_addr2 = address(0x234);
-        vm.expectRevert("Governance token: not minter");
-        ERC20Ubiquity(token_addr).mint(mock_addr1, 100);
-
-        uint256 before_bal = ERC20Ubiquity(token_addr).balanceOf(mock_addr1);
-        vm.prank(admin);
-        vm.expectEmit(true, true, false, true);
-        emit Minting(mock_addr1, admin, 100);
-        ERC20Ubiquity(token_addr).mint(mock_addr1, 100);
-        uint256 after_bal = ERC20Ubiquity(token_addr).balanceOf(mock_addr1);
-        assertEq(after_bal - before_bal, 100);
-
-        // test onlyPauser and check if transfer reverts
-        vm.expectRevert("Governance token: not pauser");
-        ERC20Ubiquity(token_addr).pause();
-        vm.prank(admin);
-        ERC20Ubiquity(token_addr).pause();
-        vm.prank(mock_addr1);
-        vm.expectRevert("Pausable: paused");
-        ERC20Ubiquity(token_addr).transfer(mock_addr2, 10);
-
-        vm.prank(admin);
-        ERC20Ubiquity(token_addr).unpause();
-
-        // test burn, onlyBurner and burnFrom
-        before_bal = ERC20Ubiquity(token_addr).balanceOf(mock_addr1);
-        vm.prank(mock_addr1);
-        vm.expectEmit(true, false, false, true);
-        emit Burning(mock_addr1, 10);
-        ERC20Ubiquity(token_addr).burn(10);
-        after_bal = ERC20Ubiquity(token_addr).balanceOf(mock_addr1);
-        assertEq(before_bal - after_bal, 10);
-
-        vm.prank(mock_addr2);
-        vm.expectRevert("Governance token: not burner");
-        ERC20Ubiquity(token_addr).burnFrom(mock_addr1, 10);
-
-        before_bal = ERC20Ubiquity(token_addr).balanceOf(mock_addr1);
-        vm.prank(admin);
-        UbiquityAlgorithmicDollarManager(uad_manager_addr).grantRole(keccak256("UBQ_BURNER_ROLE"), mock_addr2);
-
-        vm.prank(mock_addr2);
-        vm.expectEmit(true, false, false, true);
-        emit Burning(mock_addr1, 10);
-        ERC20Ubiquity(token_addr).burnFrom(mock_addr1, 10);
-        after_bal = ERC20Ubiquity(token_addr).balanceOf(mock_addr1);
-        assertEq(before_bal - after_bal, 10);
-
+    function test_mint() public {
+        
     }
 }
