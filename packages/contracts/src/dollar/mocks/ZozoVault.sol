a71228663f4e1d121934e4420f825b16b6b6c49a
0xCodercrane <108444211+0xcodercrane@users.noreply.github.com>
2022-10-11 18:13:21 +0800

    recreate mockerc20 and fix openzeppelin path issue

--- packages/contracts/src/dollar/mocks/ZozoVault.sol
+++ packages/contracts/src/dollar/mocks/ZozoVault.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.8.0;
 
 import '@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol';
 
-import 'openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract ZozoVault is ERC4626 {
   constructor(ERC20 _asset) ERC4626(_asset) ERC20('ZoZo Vault', 'vZOZO') {}
