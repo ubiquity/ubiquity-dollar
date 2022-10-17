3af10c5893e2299a219b6f34274bfd069d5da371
0xCodercrane <108444211+0xcodercrane@users.noreply.github.com>
2022-10-18 02:51:10 +0800

    replace odd with a ganache public mnemonic

--- packages/contracts/scripts/shared/constants/mnemonic.ts
+++ packages/contracts/scripts/shared/constants/mnemonic.ts
@@ -1,1 +1,1 @@
-export const TEST_MNEMONIC = "test test test test test test test test test test test junk"
+export const TEST_MNEMONIC = "horn hammer original lemon chapter weird gun pond fortune blush cupboard cat" // MUST use this mnemonic only on testnets
