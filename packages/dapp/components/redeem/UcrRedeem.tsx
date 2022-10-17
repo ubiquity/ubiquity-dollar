80b0c04ccf5030353e6a408f3482fab7b09aa2b6
sunny0714 <sun2020victory@gmail.com>
2022-10-17 08:32:40 -0700

    Recovered Router implementation for sequent swap of uCR->uAD->USDC when above peg

--- packages/dapp/components/redeem/UcrRedeem.tsx
+++ packages/dapp/components/redeem/UcrRedeem.tsx
@@ -11,6 +11,7 @@ import useTransactionLogger from "../lib/hooks/useTransactionLogger";
 import useWalletAddress from "../lib/hooks/useWalletAddress";
 import Button from "../ui/Button";
 import PositiveNumberInput from "../ui/PositiveNumberInput";
+import useRouter from "../lib/hooks/useRouter";
 import { SWAP_WIDGET_TOKEN_LIST } from "@/lib/utils";
 
 const UcrRedeem = () => {
@@ -22,6 +23,8 @@ const UcrRedeem = () => {
   const managedContracts = useManagerManaged();
 
   const [inputVal, setInputVal] = useState("0");
+  const [selectedRedeemToken, setSelectedRedeemToken] = useState("uAD");
+  const quotePrice = useRouter(selectedRedeemToken, inputVal);
 
   if (!walletAddress || !signer) {
     return <span>Connect wallet</span>;
@@ -60,12 +63,35 @@ const UcrRedeem = () => {
     setInputVal(parseInt(creditTokenValue).toString());
   };
 
+  function onChangeValue(e: React.ChangeEvent<HTMLInputElement>) {
+    setSelectedRedeemToken(e.target.value);
+  }
+
   return (
     <div>
+      <div onChange={onChangeValue}>
+        <p>Please select a token to redeem for:</p>
+        <input type="radio" id="tokenChoice1" name="redeemToken" value="uAD" checked={selectedRedeemToken === "uAD"} readOnly />
+        <label htmlFor="tokenChoice1">uAD</label>
+
+        <input type="radio" id="tokenChoice2" name="redeemToken" value="USDC" checked={selectedRedeemToken === "USDC"} readOnly />
+        <label htmlFor="tokenChoice2">USDC</label>
+
+        <input type="radio" id="tokenChoice3" name="redeemToken" value="DAI" checked={selectedRedeemToken === "DAI"} readOnly />
+        <label htmlFor="tokenChoice3">DAI</label>
+
+        <input type="radio" id="tokenChoice4" name="redeemToken" value="USDT" checked={selectedRedeemToken === "USDT"} readOnly />
+        <label htmlFor="tokenChoice4">USDT</label>
+      </div>
       <div>
         <PositiveNumberInput placeholder="uCR Amount" value={inputVal} onChange={setInputVal} />
         <span onClick={handleMax}>MAX</span>
       </div>
+      {inputVal && quotePrice && (
+        <div>
+          {inputVal} uCR -&gt; {quotePrice} {selectedRedeemToken}.
+        </div>
+      )}
       <Button onClick={handleRedeem} disabled={!submitEnabled}>
         Redeem uCR for uAD
       </Button>
