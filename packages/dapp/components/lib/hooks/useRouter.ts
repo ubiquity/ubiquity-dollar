2a84cf9ef505c8fed40d29e052744fb7267eedc2
sunny0714 <sun2020victory@gmail.com>
2022-10-18 11:36:45 -0700

    Added useTrade hook to swap uAD for other stable coins and Updated redeemUcr function

--- packages/dapp/components/lib/hooks/useRouter.ts
+++ packages/dapp/components/lib/hooks/useRouter.ts
@@ -2,41 +2,41 @@ import { useEffect, useState } from "react";
 import { AlphaRouter } from "@uniswap/smart-order-router";
 import { Percent, TradeType } from "@uniswap/sdk-core";
 import useWeb3 from "@/components/lib/hooks/useWeb3";
-import { uAD, uCR, USDC, USDT, DAI, parseAmount } from "../utils";
+import { uAD_TOKEN, uCR_TOKEN, USDC_TOKEN, USDT_TOKEN, DAI_TOKEN, parseAmount } from "../utils";
 
 const useRouter = (selectedToken: string, amountIn = "0"): [string | undefined, string | undefined] => {
   const [{ provider, walletAddress }] = useWeb3();
-  const [quotePrice, setQuotePrice] = useState<string | undefined>();
-  const [lastQuotePrice, setLastQuotePrice] = useState<string | undefined>();
+  const [quoteAmount, setQuoteAmount] = useState<string | undefined>();
+  const [lastQuoteAmount, setLastQuoteAmount] = useState<string | undefined>();
 
   async function getQuote() {
     let selectedTokenObject;
-    const parsedAmountIn = parseAmount(amountIn, uCR);
+    const parsedAmountIn = parseAmount(amountIn, uCR_TOKEN);
 
     if (selectedToken === "USDC") {
-      selectedTokenObject = USDC;
+      selectedTokenObject = USDC_TOKEN;
     } else if (selectedToken === "DAI") {
-      selectedTokenObject = DAI;
+      selectedTokenObject = DAI_TOKEN;
     } else if (selectedToken === "USDT") {
-      selectedTokenObject = USDT;
+      selectedTokenObject = USDT_TOKEN;
     } else {
-      selectedTokenObject = uAD;
+      selectedTokenObject = uAD_TOKEN;
     }
 
     if (provider && walletAddress) {
       const router = new AlphaRouter({ chainId: 1, provider: provider });
 
-      const route1 = await router.route(parsedAmountIn, uAD, TradeType.EXACT_INPUT, {
+      const route1 = await router.route(parsedAmountIn, uAD_TOKEN, TradeType.EXACT_INPUT, {
         recipient: walletAddress,
         slippageTolerance: new Percent(5, 100),
         deadline: Math.floor(Date.now() / 1000 + 1800),
       });
-      setQuotePrice(route1?.quote.toFixed(2));
+      setQuoteAmount(route1?.quote.toFixed(2));
 
       console.log("Expected uAD Value : ", route1?.quote.toFixed(2));
 
       if (route1 && selectedToken !== "uAD") {
-        const parsed_uAD_amount = parseAmount(route1.quote.toFixed(2), uAD);
+        const parsed_uAD_amount = parseAmount(route1.quote.toFixed(2), uAD_TOKEN);
 
         const route2 = await router.route(parsed_uAD_amount, selectedTokenObject, TradeType.EXACT_INPUT, {
           recipient: walletAddress,
@@ -44,7 +44,7 @@ const useRouter = (selectedToken: string, amountIn = "0"): [string | undefined, 
           deadline: Math.floor(Date.now() / 1000 + 1800),
         });
         console.log(`Expected ${selectedToken} Value : `, route2?.quote.toFixed(2));
-        setLastQuotePrice(route2?.quote.toFixed(2));
+        setLastQuoteAmount(route2?.quote.toFixed(2));
       }
     }
   }
@@ -53,7 +53,7 @@ const useRouter = (selectedToken: string, amountIn = "0"): [string | undefined, 
     getQuote();
   }, [selectedToken, amountIn]);
 
-  return [quotePrice, lastQuotePrice];
+  return [quoteAmount, lastQuoteAmount];
 };
 
 export default useRouter;
