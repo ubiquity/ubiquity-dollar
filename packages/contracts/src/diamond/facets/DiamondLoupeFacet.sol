842de64c6ba6d8f21c536f9fd30a5532bab67e32
sunny0714 <sun2020victory@gmail.com>
2022-11-03 09:08:09 -0700

    Replaced IERC165 with Openzepplin's one for avoiding double declaration

--- packages/contracts/src/diamond/facets/DiamondLoupeFacet.sol
+++ packages/contracts/src/diamond/facets/DiamondLoupeFacet.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import { LibDiamond } from  "../libraries/LibDiamond.sol";
 import { IDiamondLoupe } from "../interfaces/IDiamondLoupe.sol";
-import { IERC165 } from "../interfaces/IERC165.sol";
+import "@openzeppelin/contracts/interfaces/IERC165.sol";
 
 // The functions in DiamondLoupeFacet MUST be added to a diamond.
 // The EIP-2535 Diamond standard requires these functions.
--- packages/contracts/src/diamond/upgradeInitializers/DiamondInit.sol
+++ packages/contracts/src/diamond/upgradeInitializers/DiamondInit.sol
@@ -5,7 +5,7 @@ import {LibDiamond} from "../libraries/LibDiamond.sol";
 import { IDiamondLoupe } from "../interfaces/IDiamondLoupe.sol";
 import { IDiamondCut } from "../interfaces/IDiamondCut.sol";
 import { IERC173 } from "../interfaces/IERC173.sol";
-import { IERC165 } from "../interfaces/IERC165.sol";
+import "@openzeppelin/contracts/interfaces/IERC165.sol";
 
 // It is expected that this contract is customized if you want to deploy your diamond
 // with data from a deployment script. Use the init function to initialize state variables
