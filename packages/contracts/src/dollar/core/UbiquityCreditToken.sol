cbfcd4b6fd3bc9092ccb8e87f832c7e9825d4c1d
ryzhak <ryzhak.vladimir@gmail.com>
2022-11-29 18:48:58 +0300

    refactor: remove token symbols in UbiquityCreditToken

--- packages/contracts/src/dollar/core/UbiquityCreditToken.sol
+++ packages/contracts/src/dollar/core/UbiquityCreditToken.sol
@@ -8,7 +8,7 @@ contract UbiquityCreditToken is ERC20Ubiquity {
         ERC20Ubiquity(_manager, "Ubiquity Auto Redeem", "uAR")
     {} // solhint-disable-line no-empty-blocks
 
-    /// @notice raise capital in form of uAR (only redeemable when uAD > 1$)
+    /// @notice raise capital in form of Ubiquity Credit Token (only redeemable when Ubiquity Dollar > 1$)
     /// @param amount the amount to be minted
     /// @dev you should be minter to call that function
     function raiseCapital(uint256 amount) external {
