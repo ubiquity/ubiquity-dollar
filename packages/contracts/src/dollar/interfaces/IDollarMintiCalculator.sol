f751a1341d9972df74f73d181a14ffa83d5db2c2
Mae <mae@hashedmae.gay>
2022-12-12 18:35:55 -0800

    renaming

--- packages/contracts/src/dollar/interfaces/IDollarMintiCalculator.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.16;
-
-import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
-
-/// @title A mechanism for calculating dollars to be minted
-interface IDollarMintCalculator {
-    function getDollarsToMint() external view returns (uint256);
-}
