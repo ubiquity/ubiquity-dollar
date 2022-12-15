3e235e9af0af3910daf3a422b973fa6b0bf5b1dd
Mae <mae@hashedmae.gay>
2022-12-14 19:11:29 -0800

    bug fix

--- packages/contracts/src/dollar/ERC20Ubiquity.sol
+++ packages/contracts/src/dollar/ERC20Ubiquity.sol
@@ -13,12 +13,7 @@ import "./interfaces/IERC20Ubiquity.sol";
 /// - ERC20 minter, burner and pauser
 /// - draft-ERC20 permit
 /// - Ubiquity Manager access control
-contract ERC20Ubiquity is
-    IERC20Ubiquity,
-    ERC20,
-    ERC20Burnable,
-    ERC20Pausable
-{
+contract ERC20Ubiquity is IERC20Ubiquity, ERC20, ERC20Burnable, ERC20Pausable {
     UbiquityDollarManager public manager;
 
     // solhint-disable-next-line var-name-mixedcase
@@ -64,9 +59,11 @@ contract ERC20Ubiquity is
         _;
     }
 
-    constructor(address _manager, string memory name_, string memory symbol_)
-        ERC20(name_, symbol_)
-    {
+    constructor(
+        address _manager,
+        string memory name_,
+        string memory symbol_
+    ) ERC20(name_, symbol_) {
         _tokenName = name_;
         _symbol = symbol_;
         manager = UbiquityDollarManager(_manager);
@@ -154,7 +151,7 @@ contract ERC20Ubiquity is
     /// @param amount the amount to burn
     function burn(uint256 amount)
         public
-        override (ERC20Burnable, IERC20Ubiquity)
+        override(ERC20Burnable, IERC20Ubiquity)
         whenNotPaused
     {
         super.burn(amount);
@@ -166,7 +163,7 @@ contract ERC20Ubiquity is
     /// @param amount the amount to burn
     function burnFrom(address account, uint256 amount)
         public
-        override (ERC20Burnable, IERC20Ubiquity)
+        override(ERC20Burnable, IERC20Ubiquity)
         onlyBurner
         whenNotPaused // to suppress ? if BURNER_ROLE should do it even paused ?
     {
@@ -175,10 +172,8 @@ contract ERC20Ubiquity is
     }
 
     // @dev Creates `amount` new tokens for `to`.
-    function mint(address to, uint256 amount)
-        public
-        override
-        onlyMintercreditToken.trans
+    function mint(address to, uint256 amount) public override onlyMinter {
+        _mint(to, amount);
         emit Minting(to, msg.sender, amount);
     }
 
@@ -195,7 +190,7 @@ contract ERC20Ubiquity is
     /**
      * @dev Returns the name of the token.
      */
-    function name() public view override (ERC20) returns (string memory) {
+    function name() public view override(ERC20) returns (string memory) {
         return _tokenName;
     }
 
@@ -203,24 +198,23 @@ contract ERC20Ubiquity is
      * @dev Returns the symbol of the token, usually a shorter version of the
      * name.
      */
-    function symbol() public view override (ERC20) returns (string memory) {
+    function symbol() public view override(ERC20) returns (string memory) {
         return _symbol;
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 amount)
-        internal
-        virtual
-        override (ERC20, ERC20Pausable)
-    {
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override(ERC20, ERC20Pausable) {
         super._beforeTokenTransfer(from, to, amount);
     }
 
-    function _transfer(address sender, address recipient, uint256 amount)
-        internal
-        virtual
-        override
-        whenNotPaused
-    {
+    function _transfer(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) internal virtual override whenNotPaused {
         super._transfer(sender, recipient, amount);
     }
 }
