8ad5bdf318383637b52299ad492f9637e3d45e4a
Mae <mae@hashedmae.gay>
2022-12-14 20:06:20 -0800

    again

--- packages/dapp/components/bonds/lib/hooks/useLaunchPartyContracts.ts
+++ packages/dapp/components/bonds/lib/hooks/useLaunchPartyContracts.ts
@@ -39,8 +39,8 @@ const useLaunchPartyContracts = (): [Contracts | null, Contract[], { isSaleContr
       const simpleBond = getSimpleBondContract(SimpleBondAddress, provider);
       const rewardToken = await simpleBond.tokenRewards();
       const contracts = {
-        ubiquiStick: getUbiquiStickContract(UbiquiStickAddress, provider),
-        ubiquiStickSale: getUbiquiStickSaleContract(UbiquiStickSaleAddress, provider),
+        ubiquiStick: getUbiquityStickContract(UbiquiStickAddress, provider),
+        ubiquiStickSale: getUbiquityStickSaleContract(UbiquiStickSaleAddress, provider),
         simpleBond,
         rewardToken: getERC20Contract(rewardToken, provider),
         chainLink: getChainlinkPriceFeedContract(ChainLinkEthUsdAddress, provider),
