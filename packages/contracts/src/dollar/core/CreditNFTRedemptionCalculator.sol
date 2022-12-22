a7abdb71de1aa4e75a2acab2b39d03977bd75b53
Mae <mae@hashedmae.gay>
2022-12-22 10:53:31 -0800

    fix(contracts): error capitalization

--- packages/contracts/src/dollar/core/CreditNFTRedemptionCalculator.sol
+++ packages/contracts/src/dollar/core/CreditNFTRedemptionCalculator.sol
@@ -31,7 +31,7 @@ contract CreditNFTRedemptionCalculator is ICreditNFTRedemptionCalculator {
         require(
             CreditNFT(manager.creditNFTAddress()).getTotalOutstandingDebt()
                 < IERC20(manager.dollarTokenAddress()).totalSupply(),
-            "CreditNFT to dollar: DEBT_TOO_HIGH"
+            "CreditNFT to Dollar: DEBT_TOO_HIGH"
         );
         bytes16 one = uint256(1).fromUInt();
         bytes16 totalDebt = CreditNFT(manager.creditNFTAddress())
