a1847d94e7d646dfa3cab6735dd1e8ea1c2c9859
Steveantor <109317559+Steveantor@users.noreply.github.com>
2022-12-28 09:39:51 +0000

    fix: path names

--- packages/contracts/scripts/deploy/manager.ts
+++ packages/contracts/scripts/deploy/manager.ts
@@ -25,91 +25,91 @@ export const standardHandler = {
 export const Deploy_Manager: Deploy_Manager_Type = {
   DirectGovernanceFarmer: (args: CMDType) => {
     const { manager, base3Pool, depositZap } = args;
-    createHandler([manager, base3Pool, depositZap], args, `${dPath}/DirectGovernanceFarmer.sol:DirectGovernanceFarmer`);
+    createHandler([manager, base3Pool, depositZap], args, `${dollarPath}/DirectGovernanceFarmer.sol:DirectGovernanceFarmer`);
   },
   Erc20Ubiquity: (args: CMDType) => {
     const { manager, name, symbol } = args;
-    createHandler([manager, name, symbol], args, `${dPath}/ERC20Ubiquity.sol:ERC20Ubiquity`);
+    createHandler([manager, name, symbol], args, `${dollarPath}/ERC20Ubiquity.sol:ERC20Ubiquity`);
   },
   ERC1155Ubiquity: (args: CMDType) => {
     const { manager, uri } = args;
-    createHandler([manager, uri], args, `${dPath}/ERC1155Ubiquity.sol:ERC1155Ubiquity`);
+    createHandler([manager, uri], args, `${dollarPath}/ERC1155Ubiquity.sol:ERC1155Ubiquity`);
   },
   Staking: (args: CMDType) => {
     const { manager, stakingFormulasAddress, originals, lpBalances, weeks } = args;
-    createHandler([manager, stakingFormulasAddress, originals, lpBalances, weeks], args, `${dPath}/Staking.sol:Staking`);
+    createHandler([manager, stakingFormulasAddress, originals, lpBalances, weeks], args, `${dollarPath}/Staking.sol:Staking`);
   },
   StakingFormulas: (args: CMDType) => {
-    createHandler([], args, `${dPath}/StakingFormulas.sol:StakingFormulas`);
+    createHandler([], args, `${dollarPath}/StakingFormulas.sol:StakingFormulas`);
   },
   StakingShare: (args: CMDType) => {
     const { manager, uri } = args;
-    createHandler([manager, uri], args, `${dPath}/StakingShare.sol:StakingShare`);
+    createHandler([manager, uri], args, `${dollarPath}/StakingShare.sol:StakingShare`);
   },
   SushiSwapPool: (args: CMDType) => {
     const { manager } = args;
-    createHandler([manager], args, `${dPath}/SushiSwapPool.sol:SushiSwapPool`);
+    createHandler([manager], args, `${dollarPath}/SushiSwapPool.sol:SushiSwapPool`);
   },
   UbiquityChef: (args: CMDType) => {
     const { manager, tos, amounts, stakingShareIDs } = args;
-    createHandler([manager, tos, amounts, stakingShareIDs], args, `${dPath}/UbiquityChef.sol:UbiquityChef`);
+    createHandler([manager, tos, amounts, stakingShareIDs], args, `${dollarPath}/UbiquityChef.sol:UbiquityChef`);
   },
   UbiquityFormulas: (args: CMDType) => {
-    createHandler([], args, `${dPath}/UbiquityFormulas.sol:UbiquityFormulas`);
+    createHandler([], args, `${dollarPath}/UbiquityFormulas.sol:UbiquityFormulas`);
   },
   CreditNFT: (args: CMDType) => {
     const { manager } = args;
-    createHandler([manager], args, `${cPath}/CreditNFT.sol:CreditNFT`);
+    createHandler([manager], args, `${corePath}/CreditNFT.sol:CreditNFT`);
   },
   CreditNFTManager: (args: CMDType) => {
     const { manager, creditNFTLengthBlocks } = args;
-    createHandler([manager, creditNFTLengthBlocks], args, `${cPath}/CreditNFTManager.sol:CreditNFTManager`);
+    createHandler([manager, creditNFTLengthBlocks], args, `${corePath}/CreditNFTManager.sol:CreditNFTManager`);
   },
   CreditNFTRedemptionCalculator: (args: CMDType) => {
     const { manager } = args;
-    createHandler([manager], args, `${cPath}/CreditNFTRedemptionCalculator.sol:CreditNFTRedemptionCalculator`);
+    createHandler([manager], args, `${corePath}/CreditNFTRedemptionCalculator.sol:CreditNFTRedemptionCalculator`);
   },
   CreditRedemptionCalculator: (args: CMDType) => {
     const { manager } = args;
-    createHandler([manager], args, `${cPath}/CreditRedemptionCalculator.sol:CreditRedemptionCalculator`);
+    createHandler([manager], args, `${corePath}/CreditRedemptionCalculator.sol:CreditRedemptionCalculator`);
   },
   DollarMintCalculator: (args: CMDType) => {
     const { manager } = args;
-    createHandler([manager], args, `${cPath}/DollarMintCalculator.sol:DollarMintCalculator`);
+    createHandler([manager], args, `${corePath}/DollarMintCalculator.sol:DollarMintCalculator`);
   },
   DollarMintExcess: (args: CMDType) => {
     const { manager } = args;
-    createHandler([manager], args, `${cPath}/DollarMintExcess.sol:DollarMintExcess`);
+    createHandler([manager], args, `${corePath}/DollarMintExcess.sol:DollarMintExcess`);
   },
   TWAPOracleDollar3pool: (args: CMDType) => {
     const { pool, dollarToken0, curve3CRVToken1 } = args;
-    createHandler([pool, dollarToken0, curve3CRVToken1], args, `${cPath}/TWAPOracleDollar3pool.sol:TWAPOracleDollar3pool`);
+    createHandler([pool, dollarToken0, curve3CRVToken1], args, `${corePath}/TWAPOracleDollar3pool.sol:TWAPOracleDollar3pool`);
   },
   UbiquityCreditToken: (args: CMDType) => {
     const { manager } = args;
-    createHandler([manager], args, `${cPath}/UbiquityCreditToken.sol:UbiquityCreditToken`);
+    createHandler([manager], args, `${corePath}/UbiquityCreditToken.sol:UbiquityCreditToken`);
   },
   UbiquityDollarManager: (args: CMDType) => {
     const { admin } = args;
-    createHandler([admin], args, `${cPath}/UbiquityDollarManager.sol:UbiquityDollarManager`);
+    createHandler([admin], args, `${corePath}/UbiquityDollarManager.sol:UbiquityDollarManager`);
   },
   UbiquityDollarToken: (args: CMDType) => {
     const { manager } = args;
-    createHandler([manager], args, `${cPath}/UbiquityDollarToken.sol:UbiquityDollarToken`);
+    createHandler([manager], args, `${corePath}/UbiquityDollarToken.sol:UbiquityDollarToken`);
   },
   UbiquityGovernanceToken: (args: CMDType) => {
     const { manager } = args;
-    createHandler([manager], args, `${cPath}/UbiquityGovernanceToken.sol:UbiquityGovernanceToken`);
+    createHandler([manager], args, `${corePath}/UbiquityGovernanceToken.sol:UbiquityGovernanceToken`);
   },
   LP: (args: CMDType) => {
-    createHandler([LPTokenName, LPTokenSymbol], args, `${uPath}/LP.sol:LP`);
+    createHandler([LPTokenName, LPTokenSymbol], args, `${ubiquiStickPath}/LP.sol:LP`);
   },
   SimpleBond: (args: CMDType) => {
     standardHandler["SimpleBond"](args);
   },
   UAR: (args: CMDType) => {
     const { treasury } = args;
-    createHandler([UARName, UARSymbol, treasury], args, `${uPath}/UAR.sol:UAR`);
+    createHandler([UARName, UARSymbol, treasury], args, `${ubiquiStickPath}/UAR.sol:UAR`);
   },
   UbiquiStick: (args: CMDType) => {
     //unfinished code
