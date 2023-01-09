53b40102fd5d748dd92364955c81e7f7c82ef285
Mae <mae@hashedmae.gay>
2023-01-09 13:09:59 -0800

    feat: deploy script added bash scripts for ease of use

---
+++ packages/contracts/scripts/bash/1_StartAnvil.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Starts Anvil with mainnet fork explicitly using default accounts
+anvil -f https://cloudflare-eth.com -m 'test test test test test test test test test test test junk'
---
+++ packages/contracts/scripts/bash/2_DevelopmentDeploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source .env
+
+# Impersonates an account with high 3crv token balance
+cast rpc anvil_impersonateAccount 0x72A916702BD97923E55D78ea5A3F413dEC7F7F85 -r http://localhost:8545
+
+# Transfers tokens to default first account for Anvil
+cast send 0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490 0xa9059cbb000000000000000000000000f39fd6e51aad88f6f4ce6ab8827279cfffb9226600000000000000000000000000000000000000000000021e19e0c9bab2400000 --from 0x72A916702BD97923E55D78ea5A3F413dEC7F7F85
+
+# Ends account impersonation
+cast rpc anvil_stopImpersonatingAccount 0x72A916702BD97923E55D78ea5A3F413dEC7F7F85 -r http://localhost:8545
+
+# Deploys Protocol to Anvil Localchain
+forge script ../../scripts/deploy/dollar/solidityScripting/DevelopmentDeploy.s.sol --fork-url http://localhost:8545 --broadcast
