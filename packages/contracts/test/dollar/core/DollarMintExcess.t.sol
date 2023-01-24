3206b1f19d7007e75f6f026bf9134a64d60e21fd
rndquu <rndquu@gmail.com>
2023-01-24 11:04:49 +0300

    test: add tests for DollarMintExcess

--- packages/contracts/test/dollar/core/DollarMintExcess.t.sol
+++ packages/contracts/test/dollar/core/DollarMintExcess.t.sol
@@ -1,118 +1,216 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.3;
 
-import {IUniswapV2Router01} from "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router01.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {UbiquityDollarManager} from "../../../src/dollar/core/UbiquityDollarManager.sol";
-import {TWAPOracleDollar3pool} from "../../../src/dollar/core/TWAPOracleDollar3pool.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IUniswapV2Router01} from "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router01.sol";
 import {DollarMintExcess} from "../../../src/dollar/core/DollarMintExcess.sol";
+import {UbiquityDollarManager} from "../../../src/dollar/core/UbiquityDollarManager.sol";
 import {IMetaPool} from "../../../src/dollar/interfaces/IMetaPool.sol";
 
 import "../../helpers/LocalTestHelper.sol";
 
-contract DollarMintExcessTest is LocalTestHelper {
-    address dollarManagerAddress;
-    address dollarAddress;
+// exposes internal methods as external for testing
+contract DollarMintExcessHarness is DollarMintExcess {
+    constructor(address _manager) DollarMintExcess(_manager) {}
 
-    address twapOracleAddress;
-    address excessDollarsDistributorAddress;
-    address _sushiSwapRouter = 0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F;
-
-    function setUp() public {
-        dollarManagerAddress = helpers_deployUbiquityDollarManager();
-        twapOracleAddress = UbiquityDollarManager(dollarManagerAddress)
-            .twapOracleAddress();
-        dollarAddress = UbiquityDollarManager(dollarManagerAddress)
-            .dollarTokenAddress();
-        excessDollarsDistributorAddress = address(
-            new DollarMintExcess(dollarManagerAddress)
-        );
+    function exposed_swapDollarsForGovernance(
+        bytes16 amountIn
+    ) external returns (uint256) {
+        return _swapDollarsForGovernance(amountIn);
     }
 
-    function mockSushiSwapRouter(uint256 _expected_swap_amount) public {
-        vm.etch(_sushiSwapRouter, new bytes(0x42));
+    function exposed_governanceBuyBackLPAndBurn(uint256 amount) external {
+        _governanceBuyBackLPAndBurn(amount);
+    }
+
+    function exposed_convertToCurveLPAndTransfer(
+        uint256 amount
+    ) external returns (uint256) {
+        return _convertToCurveLPAndTransfer(amount);
+    }
+}
+
+contract DollarMintExcessTest is LocalTestHelper {
+    UbiquityDollarManager dollarManager;
+    DollarMintExcessHarness dollarMintExcess;
+
+    address sushiSwapRouterAddress = 0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F;
+
+    function setUp() public {
+        dollarManager = UbiquityDollarManager(
+            helpers_deployUbiquityDollarManager()
+        );
+        dollarMintExcess = new DollarMintExcessHarness(address(dollarManager));
+    }
+
+    function testDistributeDollars_ShouldTransferTokens() public {
+        // mock dollar token
+        vm.mockCall(
+            dollarManager.dollarTokenAddress(),
+            abi.encodeWithSelector(IERC20.balanceOf.selector),
+            abi.encode(200 ether)
+        );
+        vm.mockCall(
+            dollarManager.dollarTokenAddress(),
+            abi.encodeWithSelector(IERC20.transfer.selector),
+            abi.encode()
+        );
+
+        // mock router
+        vm.etch(sushiSwapRouterAddress, new bytes(0x42));
         uint256[] memory amountsOut = new uint256[](2);
         amountsOut[0] = 0;
-        amountsOut[1] = _expected_swap_amount;
+        amountsOut[1] = 2 ether;
         vm.mockCall(
-            _sushiSwapRouter,
+            sushiSwapRouterAddress,
             abi.encodeWithSelector(
                 IUniswapV2Router01.swapExactTokensForTokens.selector
             ),
             abi.encode(amountsOut)
         );
         vm.mockCall(
-            _sushiSwapRouter,
+            sushiSwapRouterAddress,
             abi.encodeWithSelector(IUniswapV2Router01.addLiquidity.selector),
-            abi.encode()
+            abi.encode(1, 1, 1)
         );
-    }
 
-    function mockManagerAddresses(
-        address _curve3PoolAddress,
-        address _stakingContractAddress
-    ) public {
-        vm.store(
-            dollarManagerAddress,
-            bytes32(uint256(13)),
-            bytes32(abi.encodePacked(_stakingContractAddress))
-        );
-        vm.store(
-            dollarManagerAddress,
-            bytes32(uint256(15)),
-            bytes32(abi.encodePacked(_curve3PoolAddress))
-        );
-        vm.mockCall(
-            _curve3PoolAddress,
-            abi.encodeWithSelector(IERC20.approve.selector),
-            abi.encode()
-        );
-    }
-
-    function mockMetaPool(
-        address _metaPoolAddress,
-        uint256 _expectedLiqAmt,
-        uint256 _expectedExchangeAmt
-    ) public {
+        // mock stable swap meta pool and curve3pool
+        address stableSwapMetaPoolAddress = address(0x01);
+        address curve3PoolTokenAddress = address(0x02);
         vm.prank(admin);
-        UbiquityDollarManager(dollarManagerAddress)
-            .setStableSwapMetaPoolAddress(_metaPoolAddress);
+        dollarManager.setStableSwapMetaPoolAddress(stableSwapMetaPoolAddress);
         vm.mockCall(
-            _metaPoolAddress,
+            stableSwapMetaPoolAddress,
             abi.encodeWithSelector(IMetaPool.exchange.selector),
-            abi.encode(_expectedExchangeAmt)
+            abi.encode(1 ether)
+        );
+        vm.store(
+            address(dollarManager),
+            bytes32(uint256(9)),
+            bytes32(abi.encode(curve3PoolTokenAddress))
+        );
+        vm.etch(curve3PoolTokenAddress, new bytes(0x42));
+        vm.mockCall(
+            curve3PoolTokenAddress,
+            abi.encodeWithSelector(IERC20.approve.selector),
+            abi.encode(true)
         );
         vm.mockCall(
-            _metaPoolAddress,
+            stableSwapMetaPoolAddress,
             abi.encodeWithSelector(IMetaPool.add_liquidity.selector),
-            abi.encode(_expectedLiqAmt)
+            abi.encode(1 ether)
+        );
+
+        // distribute
+        vm.expectCall(
+            dollarManager.dollarTokenAddress(),
+            abi.encodeCall(
+                IERC20.transfer,
+                (dollarManager.treasuryAddress(), 100 ether)
+            )
+        );
+        dollarMintExcess.distributeDollars();
+    }
+
+    function testSwapDollarsForGovernance_ShouldReturnSwapOutputAmount()
+        public
+    {
+        // mock router
+        vm.etch(sushiSwapRouterAddress, new bytes(0x42));
+        uint256[] memory amountsOut = new uint256[](2);
+        amountsOut[0] = 0;
+        amountsOut[1] = 1 ether;
+        vm.mockCall(
+            sushiSwapRouterAddress,
+            abi.encodeWithSelector(
+                IUniswapV2Router01.swapExactTokensForTokens.selector
+            ),
+            abi.encode(amountsOut)
+        );
+        // make a swap
+        assertEq(
+            dollarMintExcess.exposed_swapDollarsForGovernance(
+                bytes16(0x00000000000000000000000000000001)
+            ),
+            1 ether
         );
     }
 
-    function testFails_distributeDollarsWorks() public {
-        // TODO: To mock up the array of uint256[] for sushiswap/uniswap routerV2, we use vm.mockCall.
-        // function mockCall(address where, bytes calldata data, bytes calldata retdata) external;
-        // The problem here is that it doesn't return uint256[] even if we configure it like abi.encode(retVal) => retVal: uint256[]
-        // Once we figure it out, we should remove Fails from function name.
+    function testGovernanceBuyBackLPAndBurn_ShouldAllLiquidityToZeroAddress()
+        public
+    {
+        // mock router
+        vm.etch(sushiSwapRouterAddress, new bytes(0x42));
+        uint256[] memory amountsOut = new uint256[](2);
+        amountsOut[0] = 0;
+        amountsOut[1] = 2 ether;
+        vm.mockCall(
+            sushiSwapRouterAddress,
+            abi.encodeWithSelector(
+                IUniswapV2Router01.swapExactTokensForTokens.selector
+            ),
+            abi.encode(amountsOut)
+        );
+        vm.mockCall(
+            sushiSwapRouterAddress,
+            abi.encodeWithSelector(IUniswapV2Router01.addLiquidity.selector),
+            abi.encode(1, 1, 1)
+        );
+        // add liquidity
+        vm.expectCall(
+            sushiSwapRouterAddress,
+            abi.encodeCall(
+                IUniswapV2Router01.addLiquidity,
+                (
+                    dollarManager.dollarTokenAddress(),
+                    dollarManager.governanceTokenAddress(),
+                    0.5 ether,
+                    2 ether,
+                    0,
+                    0,
+                    address(0),
+                    block.timestamp + 100
+                )
+            )
+        );
+        dollarMintExcess.exposed_governanceBuyBackLPAndBurn(1 ether);
+    }
 
-        // mock up external calls
-        mockSushiSwapRouter(10e18);
-        mockMetaPool(address(0x55555), 10e18, 10e18);
-        mockManagerAddresses(address(0x123), address(0x456));
-        MockDollarToken(dollarAddress).mint(
-            excessDollarsDistributorAddress,
-            200e18
+    function testConvertToCurveLPAndTransfer_ShouldAddLiquidity() public {
+        // prepare mocks
+        address stableSwapMetaPoolAddress = address(0x01);
+        address curve3PoolTokenAddress = address(0x02);
+        vm.prank(admin);
+        dollarManager.setStableSwapMetaPoolAddress(stableSwapMetaPoolAddress);
+        vm.mockCall(
+            stableSwapMetaPoolAddress,
+            abi.encodeWithSelector(IMetaPool.exchange.selector),
+            abi.encode(1 ether)
         );
 
-        // 10% should be transferred to the treasury address
-        uint256 _before_treasury_bal = MockDollarToken(dollarAddress).balanceOf(
-            treasuryAddress
+        vm.store(
+            address(dollarManager),
+            bytes32(uint256(9)),
+            bytes32(abi.encode(curve3PoolTokenAddress))
+        );
+        vm.etch(curve3PoolTokenAddress, new bytes(0x42));
+        vm.mockCall(
+            curve3PoolTokenAddress,
+            abi.encodeWithSelector(IERC20.approve.selector),
+            abi.encode(true)
         );
 
-        DollarMintExcess(excessDollarsDistributorAddress).distributeDollars();
-        uint256 _after_treasury_bal = MockDollarToken(dollarAddress).balanceOf(
-            treasuryAddress
+        vm.mockCall(
+            stableSwapMetaPoolAddress,
+            abi.encodeWithSelector(IMetaPool.add_liquidity.selector),
+            abi.encode(1 ether)
         );
-        assertEq(_after_treasury_bal - _before_treasury_bal, 20e18);
+
+        // add liquidity
+        assertEq(
+            dollarMintExcess.exposed_convertToCurveLPAndTransfer(1 ether),
+            1 ether
+        );
     }
 }
