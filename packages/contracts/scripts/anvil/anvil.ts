4acace39a76e4f4d09582ef577f6aea12f15b97d
Mae <mae@hashedmae.gay>
2023-01-26 11:09:01 -0800

    feat(solidity scripting): anvil use rpc list -Anvil uses the RPC list in conf.ts if RPC_URL in .env is not added

--- packages/contracts/scripts/anvil/anvil.ts
+++ packages/contracts/scripts/anvil/anvil.ts
@@ -1,22 +1,27 @@
 import { spawn } from "child_process";
 import { loadEnv } from "../shared";
+import { RETRY_COUNT, RETRY_DELAY, TEST_PATTERN } from "../runner/conf";
+import { getRPC } from "../runner/rpcutil";
 import fs from "fs";
 import path from "path";
 
+let optimalRPC;
+
 const getUrl = async () => {
   const envPath = path.join(__dirname, "/../../.env");
-  if (!fs.existsSync(envPath)) {
-    throw new Error("Env file not found");
-  }
+
   const env = loadEnv(envPath);
 
   return env.rpcUrl;
 };
 
 (async () => {
-  const currentRPC = await getUrl();
-  console.log(`using ${currentRPC} for anvil...`);
-  const command = spawn("anvil", ["-f", currentRPC as string, "-m", "test test test test test test test test test test test junk", "--chain-id", "31337"]);
+  optimalRPC = await getUrl();
+  if (optimalRPC == "http://localhost:8545") {
+    optimalRPC = (await getRPC()).toString();
+  }
+  console.log(`using ${optimalRPC} for anvil...`);
+  const command = spawn("anvil", ["-f", optimalRPC as string, "-m", "test test test test test test test test test test test junk", "--chain-id", "31337"]);
   command.stdout.on("data", (output: any) => {
     console.log(output.toString());
   });
