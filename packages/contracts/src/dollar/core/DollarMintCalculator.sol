4bc71b02774dda965be913af26561dc037773dc9
Mae <mae@hashedmae.gay>
2023-01-27 11:56:46 -0800

    fix(slither): multiplication on result of division -DollarMintCalculator getDollarsToMint

--- packages/contracts/src/dollar/core/DollarMintCalculator.sol
+++ packages/contracts/src/dollar/core/DollarMintCalculator.sol
@@ -14,31 +14,30 @@ contract DollarMintCalculator is IDollarMintCalculator {
 
     bytes16 private immutable _one = (uint256(1 ether)).fromUInt();
     UbiquityDollarManager public immutable manager;
+    IERC20 dollarToken;
 
     /// @param _manager the address of the manager contract so we can fetch variables
     constructor(UbiquityDollarManager _manager) {
         manager = _manager;
+        dollarToken = IERC20(manager.dollarTokenAddress);
     }
 
     /// @notice returns (TWAP_PRICE  -1) * Dollar_Total_Supply
-    function getDollarsToMint() external view override returns (uint256) {
+    function getDollarsToMint()
+        external
+        view
+        override
+        returns (uint256 dollarsToMint)
+    {
         TWAPOracleDollar3pool oracle = TWAPOracleDollar3pool(
             manager.twapOracleAddress()
         );
         uint256 twapPrice = oracle.consult(manager.dollarTokenAddress());
         require(twapPrice > 1 ether, "DollarMintCalculator: not > 1");
-        return
-            twapPrice
-                .fromUInt()
-                .sub(_one)
-                .mul(
-                    (
-                        IERC20(manager.dollarTokenAddress())
-                            .totalSupply()
-                            .fromUInt()
-                            .div(_one)
-                    )
-                )
-                .toUInt();
+
+        bytes16 bytesTwap = twapPrice.fromUInt();
+        bytes16 dollarSupply = dollarToken.totalSupply().fromUInt();
+        dollarsToMint = (((bytesTwap.sub(one)).mul(dollarSupply)).div(_one))
+            .toUInt();
     }
 }
