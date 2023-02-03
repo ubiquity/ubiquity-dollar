bedd6b63a99e06605c81b856584338c38494259b
アレクサンダー.eth <gpg@pavlovcik.com>
2023-02-04 07:31:20 +0800

    feat(ui test): upgrade action file

--- packages/dapp/test/puppeteer.ts
+++ packages/dapp/test/puppeteer.ts
@@ -1,5 +1,17 @@
-export default function puppeteerTest() {
+import scrape from "./scraper-kernel/src/scrape";
+import path from "path";
+
+export default async function puppeteerTest() {
   console.log("Hello from puppeteer.ts");
+
+  const config = {
+    urls: "http://localhost:3000/",
+    pagesDirectory: path.join(__dirname, "pages/"),
+    verbose: 5,
+  };
+
+  const result = await scrape(config);
+  console.log({ result });
   process.exit(0);
 }
 
