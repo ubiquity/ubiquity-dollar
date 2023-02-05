6bbed769159273e11b5f3516866845c3aa739499
アレクサンダー.eth <gpg@pavlovcik.com>
2023-02-06 06:09:05 +0800

    fix: brute forcing this import

--- packages/contracts/test/ubiquistick/UbiquiStick.t.sol
+++ packages/contracts/test/ubiquistick/UbiquiStick.t.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.16;
 
 import "forge-std/Test.sol";
-import "node_modules/operator-filter-registry/src/DefaultOperatorFilterer.sol";
+import "../../node_modules/operator-filter-registry/src/DefaultOperatorFilterer.sol";
 
 import "../../src/ubiquistick/UbiquiStick.sol";
 
