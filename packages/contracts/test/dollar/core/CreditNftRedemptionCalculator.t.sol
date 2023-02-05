24ebe7fb4041f63c2bb5375e9cc8ca6c0b78bd7d
Steveantor <109317559+Steveantor@users.noreply.github.com>
2023-02-05 13:04:48 +0000

    refactor(files): change nft capitalization from capitalized to camelcase

---
+++ packages/contracts/test/dollar/core/CreditNftRedemptionCalculator.t.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {UbiquityDollarManager} from "../../../src/dollar/core/UbiquityDollarManager.sol";
+import {CreditNftRedemptionCalculator} from "../../../src/dollar/core/CreditNftRedemptionCalculator.sol";
+import {CreditNft} from "../../../src/dollar/core/CreditNft.sol";
+import {MockCreditNft} from "../../../src/dollar/mocks/MockCreditNft.sol";
+
+import "../../helpers/LocalTestHelper.sol";
+
+contract CreditNftRedemptionCalculatorTest is LocalTestHelper {
+    address dollarManagerAddress;
+    address creditNftCalculatorAddress;
+
+    function setUp() public override {
+        super.setUp();
+        dollarManagerAddress = address(manager);
+        creditNftCalculatorAddress =
+            address(new CreditNftRedemptionCalculator(manager));
+    }
+
+    function test_getCreditNftAmount_revertsIfDebtTooHigh() public {
+        uint256 totalSupply = IERC20(
+            UbiquityDollarManager(dollarManagerAddress).dollarTokenAddress()
+        ).totalSupply();
+        MockCreditNft(
+            UbiquityDollarManager(dollarManagerAddress).creditNftAddress()
+        ).setTotalOutstandingDebt(totalSupply + 1);
+
+        vm.expectRevert("CreditNft to Dollar: DEBT_TOO_HIGH");
+        CreditNftRedemptionCalculator(creditNftCalculatorAddress)
+            .getCreditNftAmount(0);
+    }
+
+    function test_getCreditNftAmount() public {
+        uint256 totalSupply = IERC20(
+            UbiquityDollarManager(dollarManagerAddress).dollarTokenAddress()
+        ).totalSupply();
+        MockCreditNft(
+            UbiquityDollarManager(dollarManagerAddress).creditNftAddress()
+        ).setTotalOutstandingDebt(totalSupply / 2);
+        assertEq(
+            CreditNftRedemptionCalculator(creditNftCalculatorAddress)
+                .getCreditNftAmount(10000),
+            40000
+        );
+    }
+}
