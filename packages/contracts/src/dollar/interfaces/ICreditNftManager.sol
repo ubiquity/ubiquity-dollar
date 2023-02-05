179052060e63fc14d2b0b0dc0e709edaf1143e41
Steveantor <109317559+Steveantor@users.noreply.github.com>
2023-02-05 13:04:48 +0000

    refactor(files): change nft capitalization from capitalized to camelcase

---
+++ packages/contracts/src/dollar/interfaces/ICreditNftManager.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
+
+import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
+
+/// @title A Credit Nft redemption mechanism for Credit Nft holders
+/// @notice Allows users to redeem individual Credit Nft or batch redeem Credit Nft
+/// @dev Implements IERC1155Receiver so that it can deal with redemptions
+interface ICreditNftManager is IERC1155Receiver {
+    function redeemCreditNft(address from, uint256 id, uint256 amount) external;
+
+    function exchangeDollarsForCreditNft(uint256 amount) external;
+}
