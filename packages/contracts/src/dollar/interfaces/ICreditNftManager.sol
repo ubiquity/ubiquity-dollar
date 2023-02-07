d370edd83fefb066ed31073e7974c8330e8e42d6
Steveantor <109317559+Steveantor@users.noreply.github.com>
2023-02-07 09:58:27 +0000

    refactor: update packages/contracts/src/dollar/interfaces/ICreditNftManager.sol
    Co-authored-by: アレクサンダー.eth <4975670+pavlovcik@users.noreply.github.com>

--- packages/contracts/src/dollar/interfaces/ICreditNftManager.sol
+++ packages/contracts/src/dollar/interfaces/ICreditNftManager.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.16;
 
 import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 
-/// @title A Credit Nft redemption mechanism for Credit Nft holders
+/// @title A Credit NFT redemption mechanism for Credit NFT holders
 /// @notice Allows users to redeem individual Credit Nft or batch redeem Credit Nft
 /// @dev Implements IERC1155Receiver so that it can deal with redemptions
 interface ICreditNftManager is IERC1155Receiver {
