a5199b89104a0d9caa9e5ad965feed9450bfc4d4
Steveantor <109317559+Steveantor@users.noreply.github.com>
2023-02-07 09:57:34 +0000

    refactor: update packages/contracts/src/dollar/interfaces/ICreditNftRedemptionCalculator.sol
    Co-authored-by: アレクサンダー.eth <4975670+pavlovcik@users.noreply.github.com>

--- packages/contracts/src/dollar/interfaces/ICreditNftRedemptionCalculator.sol
+++ packages/contracts/src/dollar/interfaces/ICreditNftRedemptionCalculator.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.16;
 
 import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 
-/// @title A mechanism for calculating Credit Nfts received for a dollar amount burnt
+/// @title A mechanism for calculating Credit NFTs received for a dollar amount burnt
 interface ICreditNftRedemptionCalculator {
     function getCreditNftAmount(
         uint256 dollarsToBurn
