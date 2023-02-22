bc88eae0c7b3b14ff4859c9ef500e666a4ebdd3c
Mae <mae@hashedmae.gay>
2023-02-22 12:00:36 -0800

    feat(migrate metapool): test assert statements

--- packages/contracts/test/scripts/MigrateMetapool.t.sol
+++ packages/contracts/test/scripts/MigrateMetapool.t.sol
@@ -10,6 +10,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "forge-std/Test.sol";
 
 contract MigrateMetapool is Test {
+    /// Ubiquity Admin EOA public address
     address admin = 0xefC0e701A824943b469a694aC564Aa1efF7Ab7dd;
     UbiquityDollarManager manager =
         UbiquityDollarManager(0x4DA97a8b831C345dBe6d16FF7432DF2b7b776d98);
@@ -17,44 +18,58 @@ contract MigrateMetapool is Test {
         Staking(payable(0xC251eCD9f1bD5230823F9A0F99a44A87Ddd4CA38));
     BondingShareV2 bonds =
         BondingShareV2(0x2dA07859613C14F6f05c97eFE37B9B4F212b5eF5);
+    /// Ubiquity Dollar Token (uAD)
     IERC20 dollarToken = IERC20(0x0F644658510c95CB46955e55D7BA9DDa9E9fBEc6);
+    /// Curve3 LP Token (3CRV)
     IERC20 curve3Token = IERC20(0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490);
+    /// Curve Metapool V2 uAD:3CRV
     IMetaPool v2Metapool =
         IMetaPool(0x20955CB69Ae1515962177D164dfC9522feef567E);
+    /// Curve Metapool V3 uAD:3CRV
     IMetaPool v3Metapool =
         IMetaPool(0x9558b18f021FC3cBa1c9B777603829A42244818b);
 
+    /// Forge Test Fork Identifier
     uint256 mainnet;
+    /// Forge Snapshot selector
     uint256 snapshot;
 
     function setUp() public {
-        /*string memory forkURL = vm.envString("RPC_URL");
-
-        if (
-            keccak256(abi.encodePacked(forkURL)) !=
-            keccak256(abi.encodePacked(""))
-        ) {
-            mainnet = vm.createSelectFork(forkURL);
-        } else {
-            mainnet = vm.createSelectFork("https://eth.ubq.fi/v1/mainnet");
-        }*/
+        /*
+            Forking mainnet to run the tests and then taking a snapshot of the state so we can later revert to the snapshot for comparing states after two different logic runs.
+            Fork URL currently hardcoded due to current limits of Github Actions
+        */
         mainnet = vm.createSelectFork("https://eth.ubq.fi/v1/mainnet");
         snapshot = vm.snapshot();
     }
 
     function testMigrateCompare() public {
-        uint256 v2dollarBal = dollarToken.balanceOf(address(v2Metapool));
-        uint256 v2curve3Bal = curve3Token.balanceOf(address(v2Metapool));
+        uint256 adminDollarPreBal = dollarToken.balanceOf(admin);
+        /// Ubiquity Dollar balance of V2 Metapool before migration
+        uint256 v2DollarPreBal = dollarToken.balanceOf(address(v2Metapool));
+        /// Curve3 LP Balance of V2 Metapool before migration
+        uint256 v2Curve3PreBal = curve3Token.balanceOf(address(v2Metapool));
+        /// Total amount of V2 Metapool LP tokens before migration
+        uint256 v2LP = v2Metapool.totalSupply();
 
-        uint256 v2LP = v2Metapool.balanceOf(address(staking)) +
-            v2Metapool.balanceOf(admin);
+        (
+            uint256 metaBalance,
+            uint256 metaBalanceV3,
+            uint256 adminCurve3Bal,
+            uint256 adminDollarBal,
+            uint256 lpMinted
+        ) = _migrate();
 
-        (uint256 metaBalance, uint256 metaBalanceV3) = migrate();
+        vm.roll(block.number + 1);
 
-        uint256 v3LP = v3Metapool.balanceOf(address(staking)) +
-            v3Metapool.balanceOf(admin);
-        uint256 v3dollarBal = dollarToken.balanceOf(address(v3Metapool));
-        uint256 v3curve3Bal = curve3Token.balanceOf(address(v3Metapool));
+        /// Total amount of V3 Metapool LP tokens after migration
+        uint256 v3LP = v3Metapool.totalSupply();
+        /// Ubiquity Dollar balance of V3 Metapool after migration
+        uint256 v3DollarBal = dollarToken.balanceOf(address(v3Metapool));
+        /// Curve3 LP balance of V3 Metapool after migration
+        uint256 v3Curve3Bal = curve3Token.balanceOf(address(v3Metapool));
+        uint256 v2Curve3PostBal = curve3Token.balanceOf(address(v2Metapool));
+        uint256 v2DollarPostBal = dollarToken.balanceOf(address(v2Metapool));
 
         console.log("LP Tokens in Staking before migration: ", metaBalance);
         console.log("LP Tokens in Staking after migration: ", metaBalanceV3);
@@ -62,19 +77,50 @@ contract MigrateMetapool is Test {
         console.log("Total LP Tokens Curve Metapool V3: ", v3LP);
         console.log(
             "Amount of UbiquityDollar Tokens in Curve Metapool V2: ",
-            v2dollarBal
+            v2DollarPreBal
         );
         console.log(
             "Amount of Curve3 Tokens in Curve Metapool V2: ",
-            v2curve3Bal
+            v2Curve3PreBal
         );
         console.log(
             "Amount of UbiquityDollar Tokens in Curve Metapool V3: ",
-            v3dollarBal
+            v3DollarBal
         );
         console.log(
             "Amount of Curve3 Tokens in Curve Metapool V3: ",
-            v3curve3Bal
+            v3Curve3Bal
+        );
+
+        /// V2 Metapool is unbalanced during migration so we use the lesser balance of the two tokens when adding liquidity to V3
+        if (adminCurve3Bal <= adminDollarBal) {
+            assertEq(v3DollarBal, adminCurve3Bal);
+            assertEq(v3Curve3Bal, adminCurve3Bal);
+        } else {
+            assertEq(v3DollarBal, adminDollarBal);
+            assertEq(v3Curve3Bal, adminDollarBal);
+        }
+        /// ensures enough v3 LP tokens were minted
+        assertGe(metaBalanceV3, metaBalance);
+        /// ensures all v3 LP tokens minted are deposited in Staking
+        assertEq(lpMinted, metaBalanceV3);
+        /* 
+            Compares the amount of Curve3 LP tokens in V2 Metapool before migration
+            to the amount of Curve3 LP tokens in both Metapools after migration to 
+            ensure no Curve3 LP tokens are lost during migration.
+            The Staking Contract holds the large majority of V2 LP tokens but there are some smaller holders
+            so we are unable to migrate all funds.
+        */
+
+        assertEq(v2Curve3PreBal, (v2Curve3PostBal + v3Curve3Bal));
+
+        /// Same as above but for Ubiquity Dollar Token
+        assertEq(
+            v2DollarPreBal,
+            v2DollarPostBal +
+                v3DollarBal +
+                dollarToken.balanceOf(admin) -
+                adminDollarPreBal
         );
     }
 
@@ -99,7 +145,7 @@ contract MigrateMetapool is Test {
 
         vm.revertTo(snapshot);
 
-        migrate();
+        _migrate();
 
         vm.roll(block.number + 10483200);
         BondingShareV2.Bond memory bondV3 = bonds.getBond(tokens[0]);
@@ -114,8 +160,8 @@ contract MigrateMetapool is Test {
         uint256 userDollarV3 = dollarToken.balanceOf(user) - dollarTokenPreBal;
         uint256 userLPV3 = curve3Token.balanceOf(user);
 
-        uint256 v3dollarBal = dollarToken.balanceOf(address(v3Metapool));
-        uint256 v3curve3Bal = curve3Token.balanceOf(address(v3Metapool));
+        uint256 v3DollarBal = dollarToken.balanceOf(address(v3Metapool));
+        uint256 v3Curve3Bal = curve3Token.balanceOf(address(v3Metapool));
 
         console.log("Metapool LP Tokens withdrawn pre migration: ", v2Bal);
         console.log("Metapool LP Tokens withdrawn post migration: ", v3Bal);
@@ -131,17 +177,32 @@ contract MigrateMetapool is Test {
         console.log("Curve3 Tokens withdrawn post migration: ", userLPV3);
         console.log(
             "Curve Metapool V3 UbiquityDollar balance after withdrawal: ",
-            v3dollarBal
+            v3DollarBal
         );
         console.log(
             "Curve Metapool V3 Curve3 balance after withdrawal: ",
-            v3curve3Bal
+            v3Curve3Bal
         );
+
+        assertGe(v3Bal, v2Bal);
+        assertLt(userDollarV3, userDollarV2);
     }
 
-    function migrate()
+    /// @dev internal function for migrating Curve Metapool LP Tokens contained in Staking from V2 to V3
+    /// @return metaBalance Staking Contract V2 LP Balance before migration
+    /// @return metaBalanceV3 Staking Contract V3 LP Balance after migration
+    /// @return adminCurve3Bal Admin address Curve3 LP token balance after withdrawal from V2 Curve Metapool
+    /// @return adminDollarBal Admin address Ubiquity Dollar token balance after withdrawal from V2 Curve Metapool
+    /// @return lpMinted amount of Metapool V3 LP tokens minted during migration
+    function _migrate()
         internal
-        returns (uint256 metaBalance, uint256 metaBalanceV3)
+        returns (
+            uint256 metaBalance,
+            uint256 metaBalanceV3,
+            uint256 adminCurve3Bal,
+            uint256 adminDollarBal,
+            uint256 lpMinted
+        )
     {
         vm.startPrank(admin);
         metaBalance = v2Metapool.balanceOf(address(staking));
@@ -149,15 +210,15 @@ contract MigrateMetapool is Test {
 
         v2Metapool.remove_liquidity(metaBalance, [uint256(0), uint256(0)]);
 
-        uint256 curve3Bal = curve3Token.balanceOf(admin);
-        uint256 dollarBal = dollarToken.balanceOf(admin);
+        adminCurve3Bal = curve3Token.balanceOf(admin);
+        adminDollarBal = dollarToken.balanceOf(admin);
 
         uint256 deposit;
 
-        if (curve3Bal <= dollarBal) {
-            deposit = curve3Bal;
+        if (adminCurve3Bal <= adminDollarBal) {
+            deposit = adminCurve3Bal;
         } else {
-            deposit = dollarBal;
+            deposit = adminDollarBal;
         }
 
         curve3Token.approve(address(v3Metapool), 0);
@@ -166,7 +227,11 @@ contract MigrateMetapool is Test {
         dollarToken.approve(address(v3Metapool), 0);
         dollarToken.approve(address(v3Metapool), deposit);
 
-        v3Metapool.add_liquidity([deposit, deposit], 0, address(staking));
+        lpMinted = v3Metapool.add_liquidity(
+            [deposit, deposit],
+            0,
+            address(staking)
+        );
         metaBalanceV3 = v3Metapool.balanceOf(address(staking));
 
         require(metaBalanceV3 > 0);
