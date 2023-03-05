93fdcac5fba907cbc61cccfe1f8dfabd56e85a62
アレクサンダー.eth <gpg@pavlovcik.com>
2023-03-05 23:37:48 +0800

    feat: light mode toggle

---
+++ packages/dapp/components/layout/LightDarkMode.tsx
@@ -0,0 +1,25 @@
+/**
+ * Component allows the user to toggle between light and dark mode:
+ * @returns JSX template
+ */
+export default function LightDarkMode() {
+  return (
+    <div id="LightDarkMode">
+      <button id="LightDarkModeToggle" onClick={toggleDarkMode}></button>
+    </div>
+  );
+}
+
+function toggleDarkMode() {
+  const body = document.body;
+  const lightDarkModeToggle = document.getElementById("LightDarkModeToggle");
+  const isLightMode = body.classList.contains("light-mode");
+
+  if (isLightMode) {
+    body.classList.remove("light-mode");
+    lightDarkModeToggle?.classList.remove("active");
+  } else {
+    body.classList.add("light-mode");
+    lightDarkModeToggle?.classList.add("active");
+  }
+}
