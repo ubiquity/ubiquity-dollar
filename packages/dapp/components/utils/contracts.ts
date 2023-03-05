64ca8dfc0f198ded6ad6f7136decfe648ee3a2eb
アレクサンダー.eth <gpg@pavlovcik.com>
2023-03-05 20:58:53 +0800

    fix(types): removed unused imports

--- packages/dapp/components/utils/contracts.ts
+++ packages/dapp/components/utils/contracts.ts
@@ -1,222 +1,67 @@
 /* eslint-disable no-unused-vars */
-import { ContractInterface, ethers } from "ethers";
 import { Provider } from "@ethersproject/providers";
-import UniswapV2PairABI from "../config/abis/UniswapV2Pair.json";
-import UniswapV3PoolABI from "../config/abis/UniswapV3Pool.json";
-import UniswapV3RouterABI from "../config/abis/UniswapV3Router.json";
-import ChainlinkPriceFeedABI from "../config/abis/ChainlinkPriceFeed.json";
-import ERC20ABI from "../config/abis/ERC20.json";
-import USDCTokenABI from "../config/abis/USDCToken.json";
-import DAITokenABI from "../config/abis/DAIToken.json";
-import USDTTokenABI from "../config/abis/USDTToken.json";
-import YieldProxyABI from "../config/abis/YieldProxy.json";
-import _SimpleBond from "@ubiquity/contracts/out/SimpleBond.sol/SimpleBond.json";
-import _UbiquiStick from "@ubiquity/contracts/out/UbiquiStick.sol/UbiquiStick.json";
-import _UbiquiStickSale from "@ubiquity/contracts/out/UbiquiStickSale.sol/UbiquiStickSale.json";
-import _ERC1155Ubiquity from "@ubiquity/contracts/out/ERC1155Ubiquity.sol/ERC1155Ubiquity.json";
-import _IJar from "@ubiquity/contracts/out/IJar.sol/IJar.json";
-import _DebtCouponManager from "@ubiquity/contracts/out/CreditNftManager.sol/CreditNftManager.json";
-import _ICurveFactory from "@ubiquity/contracts/out/ICurveFactory.sol/ICurveFactory.json";
-import _StakingToken from "@ubiquity/contracts/out/StakingShare.sol/StakingShare.json";
-import _Staking from "@ubiquity/contracts/out/Staking.sol/Staking.json";
+import { ContractInterface, ethers } from "ethers";
+
+// ARTIFACTS
 import _DebtCoupon from "@ubiquity/contracts/out/CreditNft.sol/CreditNft.json";
+import _DebtCouponManager from "@ubiquity/contracts/out/CreditNftManager.sol/CreditNftManager.json";
 import _DollarMintCalculator from "@ubiquity/contracts/out/DollarMintCalculator.sol/DollarMintCalculator.json";
+import _ERC1155Ubiquity from "@ubiquity/contracts/out/ERC1155Ubiquity.sol/ERC1155Ubiquity.json";
 import _ICouponsForDollarsCalculator from "@ubiquity/contracts/out/ICreditNftRedemptionCalculator.sol/ICreditNftRedemptionCalculator.json";
-import _IMetaPool from "@ubiquity/contracts/out/IMetaPool.sol/IMetaPool.json";
 import _IUARForDollarsCalculator from "@ubiquity/contracts/out/ICreditRedemptionCalculator.sol/ICreditRedemptionCalculator.json";
-import _MasterChefV2 from "@ubiquity/contracts/out/UbiquityChef.sol/UbiquityChef.json";
+import _ICurveFactory from "@ubiquity/contracts/out/ICurveFactory.sol/ICurveFactory.json";
+import _IJar from "@ubiquity/contracts/out/IJar.sol/IJar.json";
+import _IMetaPool from "@ubiquity/contracts/out/IMetaPool.sol/IMetaPool.json";
+import _SimpleBond from "@ubiquity/contracts/out/SimpleBond.sol/SimpleBond.json";
+import _Staking from "@ubiquity/contracts/out/Staking.sol/Staking.json";
+import _StakingToken from "@ubiquity/contracts/out/StakingShare.sol/StakingShare.json";
 import _SushiSwapPool from "@ubiquity/contracts/out/SushiSwapPool.sol/SushiSwapPool.json";
 import _TWAPOracle from "@ubiquity/contracts/out/TWAPOracleDollar3pool.sol/TWAPOracleDollar3pool.json";
+import _UbiquiStick from "@ubiquity/contracts/out/UbiquiStick.sol/UbiquiStick.json";
+import _UbiquiStickSale from "@ubiquity/contracts/out/UbiquiStickSale.sol/UbiquiStickSale.json";
+import _MasterChefV2 from "@ubiquity/contracts/out/UbiquityChef.sol/UbiquityChef.json";
+import _Credit from "@ubiquity/contracts/out/UbiquityCreditToken.sol/UbiquityCreditToken.json";
 import _UbiquityManager from "@ubiquity/contracts/out/UbiquityDollarManager.sol/UbiquityDollarManager.json";
 import _Dollar from "@ubiquity/contracts/out/UbiquityDollarToken.sol/UbiquityDollarToken.json";
-import _Credit from "@ubiquity/contracts/out/UbiquityCreditToken.sol/UbiquityCreditToken.json";
 import _UbiquityFormulas from "@ubiquity/contracts/out/UbiquityFormulas.sol/UbiquityFormulas.json";
 import _Governance from "@ubiquity/contracts/out/UbiquityGovernanceToken.sol/UbiquityGovernanceToken.json";
 
+// ABIs
+import ChainlinkPriceFeedABI from "../config/abis/ChainlinkPriceFeed.json";
+import DAITokenABI from "../config/abis/DAIToken.json";
+import ERC20ABI from "../config/abis/ERC20.json";
+import UniswapV2PairABI from "../config/abis/UniswapV2Pair.json";
+import UniswapV3PoolABI from "../config/abis/UniswapV3Pool.json";
+import UniswapV3RouterABI from "../config/abis/UniswapV3Router.json";
+import USDCTokenABI from "../config/abis/USDCToken.json";
+import USDTTokenABI from "../config/abis/USDTToken.json";
+import YieldProxyABI from "../config/abis/YieldProxy.json";
+
 import {
-  diamondTestSetupSol,
-  iPickleControllerSol,
-  iUbiquityGovernanceSol,
-  libCreditNftManagerSol,
-  localTestHelperSol,
-  mockShareV1Sol,
-  access,
-  AccessControl,
-  AccessControlFacet,
-  AddressUtils,
-  ChefFacet,
-  CollectableDust,
-  CollectableDustFacet,
-  CreditClock,
   CreditNft,
   CreditNftManager,
   CreditNftRedemptionCalculator,
   CreditRedemptionCalculator,
-  CreditRedemptionCalculatorFacet,
-  CurveDollarIncentive,
-  DefaultOperatorFilterer,
-  Diamond,
-  DiamondCutFacet,
-  DiamondInit,
-  DiamondLoupeFacet,
-  DiamondTestHelper,
-  DirectGovernanceFarmer,
   DollarMintCalculator,
-  DollarMintCalculatorFacet,
-  DollarMintExcess,
-  DollarMintExcessFacet,
-  DollarTokenFacet,
-  ERC1155,
-  ERC1155Burnable,
-  ERC1155BurnableSetUri,
-  ERC1155Pausable,
-  ERC1155PausableSetUri,
-  ERC1155Receiver,
-  ERC1155SetUri,
   ERC1155Ubiquity,
-  ERC1155UbiquityForDiamond,
-  ERC165,
   ERC20,
-  ERC20Burnable,
-  ERC20ForFacet,
-  ERC20Pausable,
-  ERC20Ubiquity,
-  ERC20UbiquityForDiamond,
-  ERC4626,
-  ERC721,
-  ERC721Burnable,
-  ERC721Enumerable,
-  EnumerableSet,
-  IAccessControl,
-  ICollectableDust,
-  ICreditNft,
-  ICreditNftManager,
-  ICreditNftRedemptionCalculator,
-  ICreditRedemptionCalculator,
   ICurveFactory,
-  IDepositZap,
-  IDiamondCut,
-  IDiamondLoupe,
-  IDollarMintCalculator,
-  IDollarMintExcess,
-  IERC1155,
-  IERC1155MetadataURI,
-  IERC1155Receiver,
-  IERC1155Ubiquity,
-  IERC165,
-  IERC173,
-  IERC20,
-  IERC20Metadata,
-  IERC20Ubiquity,
-  IERC4626,
-  IERC721,
-  IERC721Enumerable,
-  IERC721Metadata,
-  IERC721Receiver,
-  IIncentive,
   IJar,
-  IMasterChef,
   IMetaPool,
-  IOperatorFilterRegistry,
-  ISablier,
-  ISimpleBond,
-  IStableSwap3Pool,
-  IStaking,
-  IStakingShare,
-  ISushiBar,
-  ISushiMaker,
-  ISushiMasterChef,
-  ISushiSwapPool,
   ITWAPOracleDollar3pool,
-  IUAR,
-  IUBQManager,
-  IUbiquiStick,
-  IUbiquityChef,
-  IUbiquityDollarManager,
-  IUbiquityDollarToken,
-  IUbiquityFormulas,
-  IUniswapV2Factory,
   IUniswapV2Pair,
-  IUniswapV2Router01,
-  IUniswapV2Router02,
-  LP,
-  LibAccessControl,
-  LibChef,
-  LibCollectableDust,
-  LibDiamond,
-  LibDollar,
-  LibStaking,
-  LibTWAPOracle,
-  LiveTestHelper,
-  ManagerFacet,
-  MockBondingShareV1,
-  MockBondingShareV2,
-  MockBondingV1,
-  MockCreditNft,
-  MockCreditToken,
-  MockCurveFactory,
-  MockDollarToken,
-  MockERC20,
-  MockERC4626,
-  MockIncentive,
-  MockMetaPool,
-  MockStakingShare,
-  MockTWAPOracleDollar3pool,
-  MockUBQmanager,
-  OperatorFilterer,
-  Ownable,
-  OwnershipFacet,
-  Pausable,
-  Script,
   SimpleBond,
   Staking,
-  StakingFacet,
-  StakingFormulas,
-  StakingFormulasFacet,
   StakingShare,
-  StakingShareForDiamond,
   SushiSwapPool,
-  TWAPOracleDollar3pool,
-  TWAPOracleDollar3poolFacet,
   UbiquiStick,
   UbiquiStickSale,
   UbiquityChef,
   UbiquityCreditToken,
-  UbiquityCreditTokenForDiamond,
   UbiquityDollarManager,
   UbiquityDollarToken,
-  UbiquityDollarTokenForDiamond,
   UbiquityFormulas,
   UbiquityGovernanceToken,
-  UbiquityGovernanceTokenForDiamond,
-  UintUtils,
-  Vm,
-  ZozoVault,
-  DepositStateChef,
-  ZeroStateChef,
-  CreditNftManagerFacet,
-  CreditNftRedemptionCalculatorFacet,
-  TestDiamond,
-  DiamondSetup,
-  DirectGovernanceFarmerHarness,
-  DollarMintExcessHarness,
-  IController,
-  IUbiquityGovernanceToken,
-  LibCreditNftManager,
-  LocalTestHelper,
-  MockCreditNftRedemptionCalculator,
-  RemoteTestManagerFacet,
-  MigrateMetapool,
-  BondingShareForDiamond,
-  BondedState,
-  StickerState,
-  ZeroState,
-  DepositState,
-  DepositStateStaking,
-  ZeroStateStaking,
-  IStructureInterface,
-  UbiquiStickHarness,
-  IERC20Permit,
 } from "types";
 
 const getContract = (abi: ContractInterface, address: string, provider: Provider) => {
