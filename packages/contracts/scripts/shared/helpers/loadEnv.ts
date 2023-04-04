5f0724410e9c919751f8d70d1cf08c27579486cf
Steveantor <109317559+Steveantor@users.noreply.github.com>
2023-04-04 17:36:48 +0000

    feat(deployment): privatekey or mnemonic fallback

--- packages/contracts/scripts/shared/helpers/loadEnv.ts
+++ packages/contracts/scripts/shared/helpers/loadEnv.ts
@@ -1,24 +1,25 @@
 import dotenv from "dotenv";
-import { TEST_MNEMONIC } from "../constants";
+import { CURVE_WHALE, FALLBACK_RPC, TEST_MNEMONIC, USD3CRV_TOKEN } from "../constants";
 import { Wallet } from "ethers";
 import { warn } from "./logging";
 
-function warnIfNotSet(key: string) {
+const warnIfNotSet = (key: string) => {
   if (!process.env[key]?.length) {
     warn(`process.env.${key} not set`);
+    return false;
   } else {
     return process.env[key];
   }
-}
+};
 
 export const loadEnv = (path: string) => {
   dotenv.config({ path });
-  const rpcUrl = warnIfNotSet("RPC_URL") || "https://eth.ubq.fi/v1/mainnet";
+  const rpcUrl = warnIfNotSet("RPC_URL") || FALLBACK_RPC;
   const privateKey = warnIfNotSet("PRIVATE_KEY") || Wallet.fromMnemonic(TEST_MNEMONIC).privateKey;
   const adminAddress = warnIfNotSet("PUBLIC_KEY") || Wallet.fromMnemonic(TEST_MNEMONIC).address;
-  const curveWhale = warnIfNotSet("CURVE_WHALE") || "0x4486083589A063ddEF47EE2E4467B5236C508fDe";
-  const _3CRV = warnIfNotSet("USD3CRV_TOKEN") || "0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490";
-  const mnemonic = warnIfNotSet("MNEMONIC") || "test test test test test test test test test test test junk";
+  const curveWhale = warnIfNotSet("CURVE_WHALE") || CURVE_WHALE;
+  const _3CRV = warnIfNotSet("USD3CRV_TOKEN") || USD3CRV_TOKEN;
+  const mnemonic = warnIfNotSet("MNEMONIC") || TEST_MNEMONIC;
 
   const etherscanApiKey = warnIfNotSet("ETHERSCAN_API_KEY"); // no fallback
 
