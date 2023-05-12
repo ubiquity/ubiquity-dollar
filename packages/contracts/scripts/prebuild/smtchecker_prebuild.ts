466d18ea03ad4eb28e8c4abf000172a39c4fae14
Doc Akan <fibrinlab@gmail.com>
2023-05-12 17:50:16 +0100

    build: updated ref

--- packages/contracts/scripts/prebuild/smtchecker_prebuild.ts
+++ packages/contracts/scripts/prebuild/smtchecker_prebuild.ts
@@ -22,31 +22,32 @@ function getAllFiles(dirPath: string, arrayOfFiles: string[]): string[] {
 }
 
 function updateFoundryFile() {
-  const fileContent = fs.readFileSync(foundryFile, "utf8");
-  const contracts = getAllFiles("../../src", []).map((contract) =>
-    contract.replace("../../", "")
-  );
-
-  let contractsString = "";
-  for (const contract of contracts) {
-    const contractContent = fs.readFileSync(
-      path.join(__dirname, "../../", contract),
-      "utf8"
+    const fileContent = fs.readFileSync(foundry_file, "utf8");
+    const contracts = getAllFiles("../../src", []).map((contract) =>
+        contract.replace("../../", "")
     );
-    const namesMatched = contractContent.match(/contract\s+(\w+)\s+(is|{)/g);
-    if (namesMatched) {
-      const names = namesMatched.map((name) =>
-        name.replace(/contract|{|is/g, "").trim()
-      );
-      contractsString += `'${contract}' = [ '${names.join(", ")}' ]\n`;
-    }
-  }
 
-  const newContent = fileContent.replace(
-    /\[profile\.default\.model_checker\.contracts\](.*?)(?:(?:\r*\n){2})/s,
-    `[profile.default.model_checker.contracts]\n${contractsString}\n`
-  );
-  fs.writeFileSync(foundry_file, newContent, "utf8");
+    let contractsString = "";
+    for (const contract of contracts) {
+        const contractContent = fs.readFileSync(
+            path.join(__dirname, "../../", contract),
+            "utf8"
+        );
+
+        const namesMatched = contractContent.match(/contract\s+(\w+)\s+(is|{)/g);
+        if (namesMatched) {
+            const names = namesMatched.map((name) =>
+                name.replace(/contract|{|is/g, "").trim()
+            );
+            contractsString += `'${contract}' = [ '${names.join(", ")}' ]\n`;
+        }
+    }
+
+    const newContent = fileContent.replace(
+        /\[profile\.default\.model_checker\.contracts\](.*?)(?:(?:\r*\n){2})/s,
+        `[profile.default.model_checker.contracts]\n${contractsString}\n`
+    );
+    fs.writeFileSync(foundry_file, newContent, "utf8");
 }
 
 updateFoundryFile();
