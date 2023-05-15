495e226273a6987ba14ff26cb7137f1cc41caa29
Doc Akan <fibrinlab@gmail.com>
2023-05-15 20:42:53 +0100

    build: updated scripts

--- packages/contracts/scripts/prebuild/smtchecker_prebuild.ts
+++ packages/contracts/scripts/prebuild/smtchecker_prebuild.ts
@@ -3,51 +3,52 @@ import path from "path";
 
 const foundry_file = path.join(__dirname, "../../foundry.toml");
 
-function getAllFiles(dirPath: string, arrayOfFiles: string[]): string[] {
-    const fullPath = path.join(__dirname, dirPath);
-    const files = fs.readdirSync(fullPath);
+export const getAllFiles = (dirPath: string): string[] => {
+  const fullPath = path.join(__dirname, dirPath);
 
-    for (const file of files) {
-        const filePath = path.join(fullPath, file);
-        const fileStat = fs.statSync(filePath);
+  const files = fs.readdirSync(fullPath);
+  let arrayOfFiles: string[] = [];
 
-        if (fileStat.isDirectory()) {
-            getAllFiles(filePath, arrayOfFiles);
-        } else {
-            arrayOfFiles.push(filePath);
-        }
+  files.forEach((file) => {
+    const filePath = path.join(fullPath, file);
+    const fileStat = fs.statSync(filePath);
+
+    if (fileStat.isDirectory()) {
+      const nestedFiles = getAllFiles(path.join(dirPath, file));
+      arrayOfFiles = arrayOfFiles.concat(nestedFiles);
+    } else {
+      arrayOfFiles.push(path.join(dirPath, file));
     }
+  });
 
-    return arrayOfFiles;
-}
+  console.log(arrayOfFiles);
+  return arrayOfFiles;
+};
 
-function updateFoundryFile() {
-    const fileContent = fs.readFileSync(foundry_file, "utf8");
-    const contracts = getAllFiles("../../src", []).map((contract) =>
-        contract.replace("../../", "")
-    );
+const updateFoundryFile = () => {
+  const buffer = fs.readFileSync(foundry_file);
+  const fileContent = buffer.toString();
+  let contracts = getAllFiles("../../src");
+  contracts = contracts.map((contract: string) => contract.replace("../../", ""));
 
-    let contractsString = "";
-    for (const contract of contracts) {
-        const contractContent = fs.readFileSync(
-            path.join(__dirname, "../../", contract),
-            "utf8"
-        );
-
-        const namesMatched = contractContent.match(/contract\s+(\w+)\s+(is|{)/g);
-        if (namesMatched) {
-            const names = namesMatched.map((name) =>
-                name.replace(/contract|{|is/g, "").trim()
-            );
-            contractsString += `'${contract}' = [ '${names.join(", ")}' ]\n`;
-        }
+  let contractsString = "";
+  contracts.forEach((contract: string) => {
+    const contractContent = fs.readFileSync(path.join(__dirname, "../../", contract)).toString();
+    const namesMatched = contractContent.match(/contract\s+(\w+)\s+(is|{)/g);
+    if (namesMatched) {
+      const names = namesMatched.map((name: string) => name.replace("contract", "").replace(/{/g, "").replace("is", "").trim());
+      contractsString += `'${contract}' = [ '${names.join(", ")}' ]\n`;
     }
+  });
 
-    const newContent = fileContent.replace(
-        /\[profile\.default\.model_checker\.contracts\](.*?)(?:(?:\r*\n){2})/s,
-        `[profile.default.model_checker.contracts]\n${contractsString}\n`
-    );
-    fs.writeFileSync(foundry_file, newContent, "utf8");
-}
+  /// Problem is from here
+  const newContent = fileContent.replace(
+    /\[profile\.default\.model_checker\.contracts\](.*?)(?:(?:\r*\n){2})/s,
+    `[profile.default.model_checker.contracts]\n${contractsString}\n`
+  );
+  console.log(contractsString);
+  fs.writeFileSync(foundry_file, newContent);
+  console.log(foundry_file);
+};
 
 updateFoundryFile();
