9c5f1bdeeeb70e8c4ed6226f75f3704a6faaf4aa
Benjamin <benjamin.mateo@protonmail.com>
2023-05-16 10:02:33 +0200

    feat(lusd): update interface

--- packages/contracts/src/dollar/interfaces/IUbiquityPool.sol
+++ packages/contracts/src/dollar/interfaces/IUbiquityPool.sol
@@ -1,93 +1,48 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity >=0.8.19;
 
+import {IMetaPool} from "./IMetaPool.sol";
+
 interface IUbiquityPool {
-    function minting_fee() external returns (uint256);
-
-    function redeemCollateralBalances(address addr) external returns (uint256);
-
-    function redemption_fee() external returns (uint256);
-
-    function buyback_fee() external returns (uint256);
-
-    function recollat_fee() external returns (uint256);
-
-    function collatDollarBalance() external returns (uint256);
-
-    function availableExcessCollatDV() external returns (uint256);
-
-    function getCollateralPrice() external returns (uint256);
-
-    function setCollatETHOracle(
-        address _collateral_weth_oracle_address,
-        address _weth_address
+    function mintDollar(
+        address collateralAddress,
+        uint256 collateralAmount,
+        uint256 dollarOutMin
     ) external;
 
-    function mint1t1UAD(
-        uint256 collateral_amount,
-        uint256 UAD_out_min
+    function redeemDollar(
+        address collateralAddress,
+        uint256 dollarAmount,
+        uint256 collateralOutMin
     ) external;
 
-    function mintAlgorithmicUAD(
-        uint256 ubq_amount_d18,
-        uint256 UAD_out_min
+    function collectRedemption(address collateralAddress) external;
+
+    function addToken(
+        address collateralAddress,
+        IMetaPool collateralMetaPool
     ) external;
 
-    function mintFractionalUAD(
-        uint256 collateral_amount,
-        uint256 ubq_amount,
-        uint256 UAD_out_min
+    function setRedeemActive(
+        address collateralAddress,
+        bool notRedeemPaused
     ) external;
 
-    function redeem1t1UAD(
-        uint256 UAD_amount,
-        uint256 COLLATERAL_out_min
+    function getRedeemActive(
+        address _collateralAddress
+    ) external view returns (bool);
+
+    function setMintActive(
+        address collateralAddress,
+        bool notMintPaused
     ) external;
 
-    function redeemFractionalUAD(
-        uint256 UAD_amount,
-        uint256 UBQ_out_min,
-        uint256 COLLATERAL_out_min
-    ) external;
+    function getMintActive(
+        address _collateralAddress
+    ) external view returns (bool);
 
-    function redeemAlgorithmicUAD(
-        uint256 UAD_amount,
-        uint256 UBQ_out_min
-    ) external;
-
-    function collectRedemption() external;
-
-    function recollateralizeUAD(
-        uint256 collateral_amount,
-        uint256 UBQ_out_min
-    ) external;
-
-    function buyBackUBQ(
-        uint256 UBQ_amount,
-        uint256 COLLATERAL_out_min
-    ) external;
-
-    function toggleMinting() external;
-
-    function toggleRedeeming() external;
-
-    function toggleRecollateralize() external;
-
-    function toggleBuyBack() external;
-
-    function toggleCollateralPrice(uint256 _new_price) external;
-
-    function setPoolParameters(
-        uint256 new_ceiling,
-        uint256 new_bonus_rate,
-        uint256 new_redemption_delay,
-        uint256 new_mint_fee,
-        uint256 new_redeem_fee,
-        uint256 new_buyback_fee,
-        uint256 new_recollat_fee
-    ) external;
-
-    function setTimelock(address new_timelock) external;
-
-    function setOwner(address _owner_address) external;
+    function getRedeemCollateralBalances(
+        address account,
+        address collateralAddress
+    ) external view returns (uint256);
 }
