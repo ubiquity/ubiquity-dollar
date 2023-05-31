f9dcf08a1d02db9be25dc4d685cfe8ee86cb6695
rotcivegaf <victorfage@gmail.com>
2023-05-31 03:22:12 -0300

    Fix: Test nonReentrant

--- packages/contracts/test/diamond/DiamondInit.t.sol
+++ packages/contracts/test/diamond/DiamondInit.t.sol
@@ -2,7 +2,16 @@
 pragma solidity ^0.8.19;
 
 import {DiamondInit} from "../../src/dollar/upgradeInitializers/DiamondInit.sol";
+import {LibAppStorage} from "../../src/dollar/libraries/LibAppStorage.sol";
 import "forge-std/Test.sol";
+
+contract MockDiamondInit is DiamondInit {
+    function toCheckNonReentrant() external nonReentrant {
+        require(store.reentrancyStatus == 2, "reentrancyStatus: _NOT_ENTERED");
+
+        DiamondInitTest(msg.sender).ping();
+    }
+}
 
 contract DiamondInitTest is Test {
     DiamondInit dInit;
@@ -24,5 +33,16 @@ contract DiamondInitTest is Test {
 
         uint256 reentrancyStatus = uint256(vm.load(address(dInit), 0));
         assertEq(reentrancyStatus, 1);
+    }
+
+    function test_NonReentrant() public {
+        MockDiamondInit mockDInit = new MockDiamondInit();
+
+        vm.expectRevert("ReentrancyGuard: reentrant call");
+        mockDInit.toCheckNonReentrant();
+    }
+
+    function ping() external {
+        MockDiamondInit(msg.sender).toCheckNonReentrant();
     }
 }
