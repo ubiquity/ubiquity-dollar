cb386fe00664e457e0e956b57f14453f7a32bf98
Popa Vlad <popa.vlad94@gmail.com>
2023-06-19 16:26:36 +0300

    fix: check logs

--- utils/print_deployments_logs.js
+++ utils/print_deployments_logs.js
@@ -1,17 +1,18 @@
 //@ts-check
 module.exports = async ({ github, context, fs }) => {
   const pullRequestInfo = fs.readFileSync("./pr_number").toString("utf-8");
-  console.log({ pullRequestInfo });
+  console.log("PR INFO: " + pullRequestInfo);
   const infoSubstring = pullRequestInfo.split(",");
   const eventName = infoSubstring[0].split("=")[1];
   const pullRequestNumber = infoSubstring[1].split("=")[1] ?? 0;
   const commitSha = infoSubstring[2].split("=")[1];
   const deploymentsLog = fs.readFileSync("./deployments.log").toString("utf-8");
+  console.log("DEPLOYMENTS LOG: " + deploymentsLog);
 
   let defaultBody = deploymentsLog;
   const uniqueDeployUrl = deploymentsLog.match(/https:\/\/.+\.netlify\.app/gim);
   const botCommentsArray = [];
-  
+
   if (uniqueDeployUrl) {
     defaultBody = `[Deployment: ${new Date()}](${uniqueDeployUrl})`;
   }
