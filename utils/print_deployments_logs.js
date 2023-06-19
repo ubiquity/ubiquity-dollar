61cfbecef5d14c9e546e73c24e268752aae1f562
Popa Vlad <popa.vlad94@gmail.com>
2023-06-19 14:28:54 +0300

    fix: do additional checks

--- utils/print_deployments_logs.js
+++ utils/print_deployments_logs.js
@@ -1,11 +1,9 @@
 //@ts-check
 module.exports = async ({ github, context, fs }) => {
-  const pullRequestInfo = fs.readFileSync("./pr_number").toString("utf-8");
-  console.log({ pullRequestInfo });
-  const infoSubstring = pullRequestInfo.split(",");
-  const eventName = infoSubstring[0].split("=")[1];
-  const pullRequestNumber = infoSubstring[1].split("=")[1] ?? 0;
-  const commitSha = infoSubstring[2].split("=")[1];
+  const { payload } = context;
+  const eventName = context.eventName;
+  const pullRequestNumber = eventName === 'pull_request' ? payload.pull_request.number : 0;
+  const commitSha = payload.after;
   const deploymentsLog = fs.readFileSync("./deployments.log").toString("utf-8");
 
   let defaultBody = deploymentsLog;
@@ -33,10 +31,10 @@ module.exports = async ({ github, context, fs }) => {
     const timeObj = {};
     bodyArray.forEach((element) => {
       const timestamp = new Date(
-        element
-          .match(/Deployment:.*\(UTC\)/)[0]
-          .replace("Deployment:", "")
-          .trim()
+          element
+              .match(/Deployment:.*\(UTC\)/)[0]
+              .replace("Deployment:", "")
+              .trim()
       ).getTime();
       timeArray.push(timestamp);
       timeObj[timestamp] = element;
@@ -52,23 +50,23 @@ module.exports = async ({ github, context, fs }) => {
   const createNewCommitComment = async (body = defaultBody) => {
     body = GMTConverter(body);
     verifyInput(body) &&
-      (await github.rest.repos.createCommitComment({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        commit_sha: commitSha,
-        body: body,
-      }));
+    (await github.rest.repos.createCommitComment({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      commit_sha: commitSha,
+      body: body,
+    }));
   };
 
   const createNewPRComment = async (body = defaultBody) => {
     body = GMTConverter(body);
     verifyInput(body) &&
-      (await github.rest.issues.createComment({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        issue_number: pullRequestNumber,
-        body: body,
-      }));
+    (await github.rest.issues.createComment({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number: pullRequestNumber,
+      body: body,
+    }));
   };
 
   const editExistingPRComment = async () => {
@@ -76,12 +74,12 @@ module.exports = async ({ github, context, fs }) => {
     let commentBody = `${GMTConverter(defaultBody)}\n` + `${GMTConverter(botBody)}`;
     const sortCommentBody = sortComments(commentBody);
     verifyInput(sortCommentBody) &&
-      (await github.rest.issues.updateComment({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        comment_id: commentId,
-        body: sortCommentBody,
-      }));
+    (await github.rest.issues.updateComment({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      comment_id: commentId,
+      body: sortCommentBody,
+    }));
   };
 
   const deleteExistingPRComments = async () => {
@@ -100,66 +98,4 @@ module.exports = async ({ github, context, fs }) => {
     botCommentsArray.forEach(({ body }) => {
       commentBody = commentBody + `${GMTConverter(body)}\n`;
     });
-    const sortCommentBody = sortComments(commentBody);
-    await createNewPRComment(sortCommentBody);
-    await deleteExistingPRComments();
-  };
-
-  const processPRComments = async () => {
-    const perPage = 30;
-    let pageNumber = 1;
-    let hasMore = true;
-    const commentsArray = [];
-
-    while (hasMore) {
-      const { data: issueComments } = await github.rest.issues.listComments({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        issue_number: pullRequestNumber,
-        per_page: perPage,
-        page: pageNumber,
-      });
-      pageNumber++;
-
-      if (issueComments.length > 0) {
-        commentsArray.push(...issueComments);
-      } else {
-        hasMore = false;
-      }
-    }
-
-    if (commentsArray.length > 0) {
-      commentsArray.forEach((elem) => {
-        if (elem.user.type === "Bot" && elem.user.login === "ubiquibot[bot]") {
-          botCommentsArray.push(elem);
-        }
-      });
-      const botLen = botCommentsArray.length;
-      switch (botLen) {
-        case 0:
-          //no (bot) comments
-          createNewPRComment();
-          break;
-        case 1:
-          //single (bot) comment []
-          editExistingPRComment();
-          break;
-        default:
-          //multiple (bot) comments []
-          mergeExistingPRComments();
-          break;
-      }
-    } else {
-      //no comments (user|bot) []
-      createNewPRComment();
-    }
-  };
-
-  if (eventName == "pull_request") {
-    console.log("Creating a comment for the pull request");
-    await processPRComments();
-  } else {
-    console.log("Creating a comment for the commit");
-    await createNewCommitComment();
-  }
-};
+    const sortCommentBody = sortComments
