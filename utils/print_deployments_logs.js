3dcd36d44fa64d381dfb8699ad2489dc7ffa2d2d
Popa Vlad <popa.vlad94@gmail.com>
2023-06-19 17:01:55 +0300

    fix: fix conflicts

--- utils/print_deployments_logs.js
+++ utils/print_deployments_logs.js
@@ -1,20 +1,20 @@
 //@ts-check
 module.exports = async ({ github, context, fs }) => {
   const pullRequestInfo = fs.readFileSync("./pr_number").toString("utf-8");
-  console.log("PR INFO: " + pullRequestInfo);
+  console.log({ pullRequestInfo });
   const infoSubstring = pullRequestInfo.split(",");
   const eventName = infoSubstring[0].split("=")[1];
   const pullRequestNumber = infoSubstring[1].split("=")[1] ?? 0;
   const commitSha = infoSubstring[2].split("=")[1];
   const deploymentsLog = fs.readFileSync("./deployments.log").toString("utf-8");
-  console.log("DEPLOYMENTS LOG: " + deploymentsLog);
 
   let defaultBody = deploymentsLog;
   const uniqueDeployUrl = deploymentsLog.match(/https:\/\/.+\.netlify\.app/gim);
+  const commitHashes = deploymentsLog.match(/Commit: ([a-f0-9]+)/gim);
   const botCommentsArray = [];
 
   if (uniqueDeployUrl) {
-    defaultBody = `[Deployment: ${new Date()}](${uniqueDeployUrl})`;
+    defaultBody = `[Deployment: ${new Date()}](${uniqueDeployUrl})\n\nCommit Hashes:\n${commitHashes.join("\n")}`;
   }
 
   const verifyInput = (data) => {
