af6aeaa111d9075751ea9239f79da45b9c4e0721
アレクサンダー.eth <gpg@pavlovcik.com>
2023-06-20 21:58:39 +0900

    fix: identifier already imported

--- packages/contracts/src/dollar/utils/collectable-dust.sol
+++ packages/contracts/src/dollar/utils/collectable-dust.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import "@openzeppelin/contracts/utils/address.sol";
-import "@openzeppelin/contracts/utils/structs/enumerable-set.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// import "@openzeppelin/contracts/utils/address.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+// import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import "../interfaces/utils/i-collectable-dust.sol";
