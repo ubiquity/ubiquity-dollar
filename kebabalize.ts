03c91ea129f7dd2b14ae0f583a0ecef1b00ce876
アレクサンダー.eth <gpg@pavlovcik.com>
2023-06-20 18:25:26 +0900

    chore: kebabalize

---
+++ kebabalize.ts
@@ -0,0 +1,55 @@
+import fs from "fs";
+import path from "path";
+import _ from "lodash";
+
+function walk(dir: string, done: (err: Error | null, results?: string[]) => void) {
+  let results: string[] = [];
+  fs.readdir(dir, function (err, list) {
+    if (err) return done(err);
+    let pending = list.length;
+    if (!pending) return done(null, results);
+    list.forEach(function (file) {
+      file = path.resolve(dir, file);
+      fs.stat(file, function (err, stat) {
+        if (stat && stat.isDirectory()) {
+          walk(file, function (err, res) {
+            results = results.concat(res!);
+            if (!--pending) done(null, results);
+          });
+        } else {
+          results.push(file);
+          if (!--pending) done(null, results);
+        }
+      });
+    });
+  });
+}
+
+function renameAndReplaceFilesInDir(dir: string) {
+  walk(dir, function (err, files) {
+    if (err) throw err;
+    files!.forEach(function (file) {
+      const oldName = path.basename(file);
+      const newNameWithoutExtension = _.kebabCase(path.parse(oldName).name);
+      const extension = path.parse(oldName).ext;
+      const newName = `${newNameWithoutExtension}${extension}`;
+      const newPath = path.join(path.dirname(file), newName);
+
+      // Rename file
+      fs.renameSync(file, newPath);
+
+      // Replace all references of oldName with newName in all .ts and .tsx files
+      files!.forEach((filePath) => {
+        if (filePath.endsWith(".ts") || filePath.endsWith(".tsx")) {
+          const data = fs.readFileSync(filePath, "utf-8");
+          const regex = new RegExp(oldName, "g");
+          const result = data.replace(regex, newName);
+          fs.writeFileSync(filePath, result, "utf-8");
+        }
+      });
+    });
+  });
+}
+
+// Usage
+renameAndReplaceFilesInDir("./utils");
