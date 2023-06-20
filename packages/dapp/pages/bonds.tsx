9e20763bc476cc67852e603bca468e9b0219863c
アレクサンダー.eth <gpg@pavlovcik.com>
2023-06-21 04:40:20 +0900

    fix(fonts): still need to kebabalize but build succeeds

--- packages/dapp/pages/bonds.tsx
+++ packages/dapp/pages/bonds.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 
 import dynamic from "next/dynamic";
-const BondsContainer = dynamic(() => import("../components/bonds/app"), { ssr: false }); //@note Fix: (Hydration Error)
+const BondsContainer = dynamic(() => import("../components/bonds/_app"), { ssr: false }); //@note Fix: (Hydration Error)
 
 const Bonds: FC = (): JSX.Element => {
   return <BondsContainer />;
