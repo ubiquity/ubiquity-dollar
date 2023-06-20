e1314c62a9b75f7eb65ca3ee6d78b864b00bb5c7
アレクサンダー.eth <gpg@pavlovcik.com>
2023-06-21 04:16:19 +0900

    refactor: manual touch up

--- packages/dapp/pages/staking.tsx
+++ packages/dapp/pages/staking.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import BondingSharesExplorer from "@/components/staking/BondingSharesExplorer";
+import BondingSharesExplorer from "@/components/staking/bonding-shares-explorer";
 import dynamic from "next/dynamic";
 const WalletConnectionWall = dynamic(() => import("@/components/ui/wallet-connection-wall"), { ssr: false }); //@note Fix: (Hydration Error)
 
