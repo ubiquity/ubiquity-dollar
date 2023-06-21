cf7a4cd98907475530f5f9171cb4aa96c35b350a
アレクサンダー.eth <gpg@pavlovcik.com>
2023-06-21 18:01:23 +0900

    fix: capitalization

--- packages/dapp/components/monitor/ui/Address.tsx
+++
@@ -1,7 +0,0 @@
-const Address = (props: { address: string; title: string }): JSX.Element => (
-  <a target="_blank" rel="noopener noreferrer" title={props.title} href={`https://etherscan.io/address/${props.address}`}>
-    {props.address}
-  </a>
-);
-
-export default Address;
--- packages/dapp/components/monitor/ui/Balance.tsx
+++
@@ -1,7 +0,0 @@
-const Balance = (props: { balance: number; unit?: string; title: string }): JSX.Element => (
-  <div>
-    <span>{props.title}</span> <span>{props.balance}</span>
-  </div>
-);
-
-export default Balance;
