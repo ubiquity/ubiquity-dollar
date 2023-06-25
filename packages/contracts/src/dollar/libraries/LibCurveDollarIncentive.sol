c78226d734e13a0be1735743876478142da307cf
Paul <web3samuraidev@gmail.com>
2023-06-25 05:18:43 +0000

    test: check coverage

--- packages/contracts/src/dollar/libraries/LibCurveDollarIncentive.sol
+++ packages/contracts/src/dollar/libraries/LibCurveDollarIncentive.sol
@@ -150,15 +150,15 @@ library LibCurveDollarIncentive {
     //          when Ubiquity Dollar is <1$
     function _getPercentDeviationFromUnderPeg(
         uint256 amount
-    ) internal returns (uint256) {
+    ) internal view returns (uint256) {
         uint256 curPrice = _getTWAPPrice();
-        LibTWAPOracle.update();
         if (curPrice >= 1 ether) {
             return 0;
         }
 
         bytes16 res = _one.sub(curPrice.fromUInt()).mul(amount.fromUInt());
         // returns (1- TWAP_Price) * amount.
+        //LibTWAPOracle.update();
         return res.div(_one).toUInt();
     }
 
