06242da98e24005214a0e91c2b712cc3466d7bb3
Paul <web3samuraidev@gmail.com>
2023-07-03 23:34:18 +0000

    test: ui only

--- packages/dapp/pages/staking.tsx
+++ packages/dapp/pages/staking.tsx
@@ -1,12 +1,11 @@
 import { FC } from "react";
-import BondingSharesExplorer from "@/components/staking/bonding-shares-explorer";
 import dynamic from "next/dynamic";
 const WalletConnectionWall = dynamic(() => import("@/components/ui/wallet-connection-wall"), { ssr: false }); //@note Fix: (Hydration Error)
 
 const Staking: FC = (): JSX.Element => {
   return (
     <WalletConnectionWall>
-      <BondingSharesExplorer />
+      <span></span>
     </WalletConnectionWall>
   );
 };
