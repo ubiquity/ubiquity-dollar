625768acbfbefa2339ed8c6363d978c83152f977
Popa Vlad <popa.vlad94@gmail.com>
2023-07-03 18:30:16 +0300

    fix merge

--- utils/print-deployments-logs.js
+++ utils/print-deployments-logs.js
@@ -15,7 +15,6 @@ module.exports = async ({ github, context, fs }) => {
   if (uniqueDeployUrl) {
     defaultBody = `[Deployment: ${new Date()} | Commit: ${commitSha}](${uniqueDeployUrl})`;
   }
-    console.log("Default body: " + defaultBody);
 
   const verifyInput = (data) => {
     return data !== "";
