b20488de8ab4fad0492d7dedac3e42bb4e10237b
Paul <web3samuraidev@gmail.com>
2023-07-04 22:34:52 +0000

    test: update local data

--- packages/dapp/components/utils/local-data.ts
+++ packages/dapp/components/utils/local-data.ts
@@ -6,34 +6,24 @@ import { LOCAL_NODE_ADDRESS } from "@/components/lib/hooks/use-web-3";
 dotenv.config();
 
 export async function fetchData() {
-  const provider = new ethers.providers.JsonRpcProvider(LOCAL_NODE_ADDRESS);
-  const signer = provider.getSigner();
-
   if (process.env.DEBUG === "true") {
-    console.log(signer);
-  }
-
-  if (provider && signer) {
+    const provider = new ethers.providers.JsonRpcProvider(LOCAL_NODE_ADDRESS);
+    const signer = provider.getSigner();
     try {
       const managerAddress = "0x4e037B9A8Ce977462DA4E10Fc164363C827abfc6";
       const managerContract = await getUbiquityManagerContract(managerAddress, provider);
 
-      if (process.env.DEBUG === "true") {
-        console.log(await managerContract.connect(signer).twapOracleAddress(), "Twap Oracle Address");
-        console.log(await managerContract.connect(signer).creditCalculatorAddress(), "Credit Calculator Address");
-      }
+      console.log(await managerContract.connect(signer).twapOracleAddress(), "Twap Oracle Address");
+      console.log(await managerContract.connect(signer).creditCalculatorAddress(), "Credit Calculator Address");
+
+      const block = await provider?.getBlockNumber();
+      console.log("Anvil Block Number", block);
+
+      console.log("Connected to Chain ID", provider?.network.chainId);
     } catch (error) {
       console.log(error);
     }
   }
-
-  //if (process.env.DEBUG === 'true') {
-  //console.log(provider);
-  const block = await provider?.getBlockNumber();
-  console.log("Anvil Block Number", block);
-
-  console.log("Connected to Chain ID", provider?.network.chainId);
-  //}
 }
 
 fetchData();
