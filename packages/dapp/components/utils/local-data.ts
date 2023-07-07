6a59f766ff1b92085ad2a5ecf05b892232b9be7c
Paul <web3samuraidev@gmail.com>
2023-07-07 13:31:26 +0000

    test: update debug

--- packages/dapp/components/utils/local-data.ts
+++ packages/dapp/components/utils/local-data.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import { getUbiquityManagerContract } from "@/components/utils/contracts";
-import { ethers } from "ethers";
+import { Signer, ethers } from "ethers";
 import { LOCAL_NODE_ADDRESS } from "@/components/lib/hooks/use-web-3";
 
 dotenv.config();
@@ -15,6 +15,8 @@ export async function fetchData() {
 
       console.log(await managerContract.connect(signer).twapOracleAddress(), "Twap Oracle Address");
       console.log(await managerContract.connect(signer).creditCalculatorAddress(), "Credit Calculator Address");
+      console.log(await managerContract.connect(signer).setStakingContractAddress("0x106A75f7e416f8C9112886C21901b629A7A3076D"), "set Staking");
+      console.log(await managerContract.connect(signer).stakingContractAddress());
 
       const block = await provider?.getBlockNumber();
       console.log("Anvil Block Number", block);
