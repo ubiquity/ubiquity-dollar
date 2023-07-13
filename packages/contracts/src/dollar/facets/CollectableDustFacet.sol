f5885d579bd65c349dd2aab3ae391cb496b322b8
rndquu <rndquu@gmail.com>
2023-07-13 19:09:12 +0300

    docs: add CollectableDust docs

--- packages/contracts/src/dollar/facets/CollectableDustFacet.sol
+++ packages/contracts/src/dollar/facets/CollectableDustFacet.sol
@@ -5,16 +5,19 @@ import "../libraries/LibCollectableDust.sol";
 import {Modifiers} from "../libraries/LibAppStorage.sol";
 import {ICollectableDust} from "../../dollar/interfaces/utils/ICollectableDust.sol";
 
+/// @notice Contract for collecting dust (i.e. not part of a protocol) tokens sent to a contract
 contract CollectableDustFacet is ICollectableDust, Modifiers {
-    /// Collectable Dust
+    /// @inheritdoc ICollectableDust
     function addProtocolToken(address _token) external onlyStakingManager {
         LibCollectableDust.addProtocolToken(_token);
     }
 
+    /// @inheritdoc ICollectableDust
     function removeProtocolToken(address _token) external onlyStakingManager {
         LibCollectableDust.removeProtocolToken(_token);
     }
 
+    /// @inheritdoc ICollectableDust
     function sendDust(
         address _to,
         address _token,
--- packages/contracts/src/dollar/libraries/LibCollectableDust.sol
+++ packages/contracts/src/dollar/libraries/LibCollectableDust.sol
@@ -10,26 +10,38 @@ import {UintUtils} from "./UintUtils.sol";
 import {LibAppStorage} from "./LibAppStorage.sol";
 import "./Constants.sol";
 
+/// @notice Library for collecting dust (i.e. not part of a protocol) tokens sent to a contract
 library LibCollectableDust {
     using SafeERC20 for IERC20;
     using AddressUtils for address;
     using EnumerableSet for EnumerableSet.AddressSet;
     using UintUtils for uint256;
 
+    /// @notice Emitted when dust tokens are sent to the `_to` address
     event DustSent(address _to, address token, uint256 amount);
+
+    /// @notice Emitted when token is added to a protocol
     event ProtocolTokenAdded(address _token);
+
+    /// @notice Emitted when token is removed from a protocol
     event ProtocolTokenRemoved(address _token);
 
+    /// @notice Struct used as a storage for the current library
     struct Tokens {
         EnumerableSet.AddressSet protocolTokens;
     }
 
+    /// @notice Storage slot used to store data for this library
     bytes32 constant COLLECTABLE_DUST_CONTROL_STORAGE_SLOT =
         bytes32(
             uint256(keccak256("ubiquity.contracts.collectable.dust.storage")) -
                 1
         );
 
+    /**
+     * @notice Returns struct used as a storage for this library
+     * @return l Struct used as a storage
+     */
     function collectableDustStorage() internal pure returns (Tokens storage l) {
         bytes32 slot = COLLECTABLE_DUST_CONTROL_STORAGE_SLOT;
         assembly {
@@ -37,6 +49,10 @@ library LibCollectableDust {
         }
     }
 
+    /**
+     * @notice Adds token address to a protocol
+     * @param _token Token address to add
+     */
     function addProtocolToken(address _token) internal {
         require(
             !collectableDustStorage().protocolTokens.contains(_token),
@@ -46,6 +62,10 @@ library LibCollectableDust {
         emit ProtocolTokenAdded(_token);
     }
 
+    /**
+     * @notice Removes token address from a protocol
+     * @param _token Token address to remove
+     */
     function removeProtocolToken(address _token) internal {
         require(
             collectableDustStorage().protocolTokens.contains(_token),
@@ -55,6 +75,12 @@ library LibCollectableDust {
         emit ProtocolTokenRemoved(_token);
     }
 
+    /**
+     * @notice Sends dust tokens (which are not part of a protocol) to the `_to` address
+     * @param _to Tokens receiver address
+     * @param _token Token address to send
+     * @param _amount Amount of tokens to send
+     */
     function sendDust(address _to, address _token, uint256 _amount) internal {
         require(
             _to != address(0),
