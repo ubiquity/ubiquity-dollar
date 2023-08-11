eb727301784773b6d92832976d529070bc772e02
Paul <41552663+molecula451@users.noreply.github.com>
2023-08-11 00:08:09 +0000

    test: update prevent regex backtracking

--- packages/contracts/scripts/deploy/manager/diamond-in-all.ts
+++ packages/contracts/scripts/deploy/manager/diamond-in-all.ts
@@ -93,7 +93,7 @@ const create = async (args: ForgeArguments): Promise<{ result: DeploymentResult 
   }
   if (stdout) {
     //test regex
-    const regex = /[^{}]*(?:R[^{}]*)*}/g;
+    const regex = /[^{}]*+(?:R[^{}]*+)*+}/g;
     const found = stdout.match(regex);
     if (found && JSON.parse(found[0])?.deployedTo) {
       const { deployedTo, deployer, transactionHash } = JSON.parse(found[0]);
