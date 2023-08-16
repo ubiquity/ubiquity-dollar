546b2044c978e2c408524a34b2c0ccacf5626376
Paul <web3samuraidev@gmail.com>
2023-08-16 07:41:24 +0000

    feat: update ui hook

--- packages/dapp/components/lib/hooks/contracts/use-deployed-contracts.tsx
+++ packages/dapp/components/lib/hooks/contracts/use-deployed-contracts.tsx
@@ -1,22 +1,21 @@
 import { getDebtCouponManagerContract, getUbiquityManagerContract } from "@/components/utils/contracts";
 import { useMemo } from "react";
-import useDeployedAddress from "../use-deployed-address";
 import useWeb3Provider from "../use-web-3-provider";
+import useDeployedAddress from "../use-deployed-address";
 
 export type DeployedContracts = ReturnType<typeof useDeployedContracts> | null;
 const useDeployedContracts = () => {
   const provider = useWeb3Provider();
   // cspell: disable-next-line
-  const [addr1, addr2] = useDeployedAddress("UbiquityAlgorithmicDollarManager", "DebtCouponManager");
+  const [addr1] = useDeployedAddress("Diamond");
   return useMemo(
     () =>
-      addr1 && addr2 && provider
+      addr1 && provider
         ? {
             manager: getUbiquityManagerContract(addr1, provider),
-            debtCouponManager: getDebtCouponManagerContract(addr2, provider),
           }
         : null,
-    [addr1, addr2, provider]
+    [addr1, provider]
   );
 };
 
