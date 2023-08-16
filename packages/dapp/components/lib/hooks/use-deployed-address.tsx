85e8bc4895fe25e4f0359c7f43cfd53e6b35036b
Paul <web3samuraidev@gmail.com>
2023-08-16 23:58:11 +0000

    feat: enhace readbility

--- packages/dapp/components/lib/hooks/use-deployed-address.tsx
+++ packages/dapp/components/lib/hooks/use-deployed-address.tsx
@@ -5,22 +5,22 @@ import { useNetwork } from "wagmi";
 const LOCAL_CHAIN = 31337;
 const deployedContracts: Record<string, any> = contractDeployments;
 
+const getContractAddress = (record: Record<string, any>, name: string): string | undefined => {
+  const contractInstance = record.contracts?.[name];
+  return contractInstance?.address;
+};
+
 const useDeployedAddress = (...names: string[]): string[] => {
   const { chain } = useNetwork();
   const chainId: number = chain?.id || LOCAL_CHAIN;
-  if (chainId) {
-    const record = deployedContracts[chainId.toString()] ?? {};
 
-    const getContractAddress = (name: string): string | undefined => {
-      const contractInstance = record?.contracts ? record?.contracts[name] : undefined;
-      return contractInstance?.address || undefined;
-    };
-
-    const addresses = names.map((name) => getContractAddress(name) || "");
-    return addresses;
-  } else {
+  if (!chainId) {
     return [];
   }
+
+  const record = deployedContracts[chainId.toString()] || {};
+  const addresses = names.map((name) => getContractAddress(record, name) || "");
+  return addresses;
 };
 
 export default useDeployedAddress;
