5b44d4880dda7f41e5b2d1b7babc3270fadd612a
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2023-08-26 21:38:07 +0100

    chore: fix deploy scripts
    all deploying

--- packages/contracts/scripts/deploy/dollar/solidityScripting/05_StakingShare.s.sol
+++ packages/contracts/scripts/deploy/dollar/solidityScripting/05_StakingShare.s.sol
@@ -24,7 +24,7 @@ contract StakingShareScript is CreditScript {
         );
         IStakingShareToken = new StakingShare();
         proxyStakingShare = new UupsProxy(
-            address(stakingShare),
+            address(IStakingShareToken),
             manAndUriPayload
         );
         stakingShare = StakingShare(address(proxyStakingShare));
