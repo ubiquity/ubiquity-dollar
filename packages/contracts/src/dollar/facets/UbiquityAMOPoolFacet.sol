3a8d9cd3936dbdfd4dc4a2f5183093f3adc84e16
Doc Akan <fibrinlab@gmail.com>
2023-09-18 16:46:19 +0100

    feat: amo facet added

---
+++ packages/contracts/src/dollar/facets/UbiquityAMOPoolFacet.sol
@@ -0,0 +1,240 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.19;
+
+import {IUbiquityAMOPool} from "../../dollar/interfaces/IUbiquityAMOPool.sol";
+import {Modifiers} from "../libraries/LibAppStorage.sol";
+
+/**
+ * @notice Ubiquity AMO Pool contract based on Frax Finance
+ * @notice Inspired from Frax Finance https://github.com/FraxFinance/frax-solidity
+ */
+contract UbiquityAMOPoolFacet is IUbiquityAMOPool {
+    function collateral_information(
+        address _collat_address
+    ) external view returns (CollateralInformation memory return_data) {
+        return LibUbiquityAMOPool.collateral_information();
+    }
+
+    function allCollaterals() external view returns (address[] memory) {
+        return LibUbiquityAMOPool.allCollaterals();
+    }
+
+    function getUADPrice() public view returns (uint256) {
+        return LibUbiquityAMOPool.getUADPrice();
+    }
+
+    function getGovTokenPrice() public view returns (uint256) {
+        return LibUbiquityAMOPool.getGovTokenPrice();
+    }
+
+    function getUADInCollateral(
+        uint256 _col_idx,
+        uint256 _uad_amount
+    ) public view returns (uint256) {
+        return LibUbiquityAMOPool.getUADInCollateral();
+    }
+
+    function freeCollatBalance(uint256 _col_idx) public view returns (uint256) {
+        return LibUbiquityAMOPool.getUADInCollateral();
+    }
+
+    function collatDollarBalance() external view returns (uint256) {
+        return LibUbiquityAMOPool.collateralDollarBalance();
+    }
+
+    function buybackAvailableCollateral() external view returns (uint256) {
+        return LibUbiquityAMOPool.buybackAvailableCollateral();
+    }
+
+    function recollateralizeColAvailableE18() external view returns (uint256) {
+        return LibUbiquityAMOPool.recollateralizeColAvailableE18();
+    }
+
+    function recollateralizeAvailableGovToken() public view returns (uint256) {
+        return LibUbiquityAMOPool.recollateralizeAvailableGovToken();
+    }
+
+    function curEpochHr() public view returns (uint256) {
+        return LibUbiquityAMOPool.curEpochHr();
+    }
+
+    function mintUad(
+        uint256 _col_idx,
+        uint256 _uad_amt,
+        uint256 _uad_out_min,
+        uint256 _max_collateral_in,
+        uint256 _max_gov_token_in,
+        bool _one_to_one_override
+    )
+        external
+        returns (
+            uint256 total_uad_mint,
+            uint256 collateral_needed,
+            uint256 gov_token_needed
+        )
+    {
+        return
+            LibUbiquityAMOPool.mintUad(
+                _col_idx,
+                _uad_amt,
+                _uad_out_min,
+                _max_collateral_in,
+                _max_gov_token_in,
+                _one_to_one_override
+            );
+    }
+
+    function redeemUad(
+        uint256 _col_idx,
+        uint256 _uad_amount,
+        uint256 _gov_token_out_min,
+        uint256 _col_out_min
+    ) external returns (uint256 collateral_out, uint256 gov_token_out) {
+        return
+            LibUbiquityAMOPool.redeemUad(
+                _col_idx,
+                _uad_amount,
+                _gov_token_out_min,
+                _col_out_min
+            );
+    }
+
+    function collectRedemption(
+        uint256 _col_idx
+    ) external returns (uint256 gov_token_amount, uint256 collateral_amount) {
+        return LibUbiquityAMOPool.collectRedemption(_col_idx);
+    }
+
+    function buyBackGovToken(
+        uint256 _col_idx,
+        uint256 _gov_token_amount,
+        uint256 _col_out_min
+    ) external returns (uint256 col_out) {
+        return
+            LibUbiquityAMOPool.buyBackGovToken(
+                _col_idx,
+                _gov_token_amount,
+                _col_out_min
+            );
+    }
+
+    function recollateralize(
+        uint256 _col_idx,
+        uint256 _collateral_amount,
+        uint256 _gov_token_out_min
+    ) external returns (uint256 gov_token_out) {
+        return
+            LibUbiquityAMOPool.recollateralize(
+                _col_idx,
+                _collateral_amount,
+                _gov_token_out_min
+            );
+    }
+
+    function amoMinterBorrow(
+        uint256 _collateral_amount
+    ) external onlyAMOMinters {
+        LibUbiquityAMOPool.amoMinterBorrow(_collateral_amount);
+    }
+
+    /* ========== RESTRICTED FUNCTIONS, CUSTODIAN CAN CALL TOO ========== */
+
+    function toggleProtocol(
+        uint256 _col_idx,
+        uint8 _tog_idx
+    ) external onlyGovCustodian {
+        LibUbiquityAMOPool.toggleProtocol(_col_idx, _tog_idx);
+    }
+
+    /* ========== RESTRICTED FUNCTIONS, GOVERNANCE ONLY ========== */
+
+    function addAMOMinter(address _amo_minter_addr) external onlyByOwnGov {
+        LibUbiquityAMOPool.addAMOMinter(_amo_minter_addr);
+    }
+
+    function removeAMOMinter(address _amo_minter_addr) external onlyByOwnGov {
+        LibUbiquityAMOPool.removeAMOMinter(_amo_minter_addr);
+    }
+
+    function setCollateralPrice(
+        uint256 _col_idx,
+        uint256 _new_price
+    ) external onlyByOwnGov {
+        LibUbiquityAMOPool.setCollateralPrice(_col_idx, _new_price);
+    }
+
+    function toggleCollateral(uint256 _col_idx) external onlyByOwnGov {
+        LibUbiquityAMOPool.toggleCollateral(_col_idx);
+    }
+
+    function setPoolCeiling(
+        uint256 _col_idx,
+        uint256 _new_ceiling
+    ) external onlyByOwnGov {
+        LibUbiquityAMOPool.setPoolCeiling(_col_idx, _new_ceiling);
+    }
+
+    function setFees(
+        uint256 _col_idx,
+        uint256 _new_mint_fee,
+        uint256 _new_redeem_fee,
+        uint256 _new_buyback_fee,
+        uint256 _new_collateral_fee
+    ) external onlyByOwnGov {
+        LibUbiquityAMOPool.setFees(
+            _col_idx,
+            _new_mint_fee,
+            _new_redeem_fee,
+            _new_buyback_fee,
+            _new_collateral_fee
+        );
+    }
+
+    function setPoolParameters(
+        uint256 _new_bonus_rate,
+        uint256 _new_redemption_delay
+    ) external onlyByOwnGov {
+        LibUbiquityAMOPool.setPoolParameters(
+            _new_bonus_rate,
+            _new_redemption_delay
+        );
+    }
+
+    function setPriceThresholds(
+        uint256 _new_mint_price_threshold,
+        uint256 _new_redeem_price_threshold
+    ) external onlyByOwnGov {
+        LibUbiquityAMOPool.setPriceThresholds(
+            _new_mint_price_threshold,
+            _new_redeem_price_threshold
+        );
+    }
+
+    function setBbkRctPerHour(
+        uint256 _bbkMaxColE18OutPerHour,
+        uint256 _rctMaxFxsOutPerHour
+    ) external onlyByOwnGov {
+        LibUbiquityAMOPool.setBbkRctPerHour(
+            _bbkMaxColE18OutPerHour,
+            _rctMaxFxsOutPerHour
+        );
+    }
+
+    function setOracles(
+        address _uad_usd_chainlink_addr,
+        address _gov_token_usd_chainlink_addr
+    ) external onlyByOwnGov {
+        LibUbiquityAMOPool.setOracles(
+            _uad_usd_chainlink_addr,
+            _gov_token_usd_chainlink_addr
+        );
+    }
+
+    function setCustodian(address _new_custodian) external onlyByOwnGov {
+        LibUbiquityAMOPool.setCustodian(_new_custodian);
+    }
+
+    function setTimelock(address _new_timelock) external onlyByOwnGov {
+        LibUbiquityAMOPool.setTimelock(_new_timelock);
+    }
+}
