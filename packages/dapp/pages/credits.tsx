d9eb49db4e8984f50d7286a6c8fb2b98477f22f7
Korrrba <gitcoiner@gmail.com>
2023-10-02 13:43:43 +0200

    feat: refactor credits page to diamond contract
    Resolves: #786

--- packages/dapp/pages/credits.tsx
+++ packages/dapp/pages/credits.tsx
@@ -3,7 +3,7 @@ import DollarPrice from "@/components/redeem/dollar-price";
 import UcrRedeem from "@/components/redeem/ucr-redeem";
 import UcrNftGenerator from "@/components/redeem/debt-coupon-deposit";
 import UcrNftRedeem from "@/components/redeem/ucr-nft-redeem";
-import useManagerManaged from "@/components/lib/hooks/contracts/use-manager-managed";
+import useProtocolContracts from "@/components/lib/hooks/contracts/use-protocol-contracts";
 import useEffectAsync from "@/components/lib/hooks/use-effect-async";
 // import DisabledBlurredMessage from "@/components/ui/DisabledBlurredMessage";
 import dynamic from "next/dynamic";
@@ -11,17 +11,18 @@ const WalletConnectionWall = dynamic(() => import("@/components/ui/wallet-connec
 
 const PriceStabilization: FC = (): JSX.Element => {
   const [twapInteger, setTwapInteger] = useState<number>(0);
-  const managedContracts = useManagerManaged();
+  const protocolContracts = useProtocolContracts();
 
   useEffectAsync(async () => {
-    if (managedContracts) {
-      const twapPrice = await managedContracts.dollarTwapOracle.consult(managedContracts.dollarToken.address);
+    if (protocolContracts) {
+      const dollarTokenAddress = protocolContracts.managerFacet && (await protocolContracts.managerFacet.dollarTokenAddress());
+      const twapPrice = protocolContracts.twapOracleDollar3poolFacet && (await protocolContracts.twapOracleDollar3poolFacet.consult(dollarTokenAddress));
       if (twapPrice) {
         const twapPriceInteger = (twapPrice as unknown as number) / 1e18;
         setTwapInteger(twapPriceInteger);
       }
     }
-  }, [managedContracts]);
+  }, [protocolContracts]);
 
   return (
     <WalletConnectionWall>
