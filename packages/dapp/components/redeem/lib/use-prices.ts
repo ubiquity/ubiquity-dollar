df8cefa15341fba0ece2cbd5dea5aba7638369e4
bojan07 <bojantorma@gmail.com>
2023-10-03 05:39:24 -0400

    fix: added debug option to refreshPrices function

--- packages/dapp/components/redeem/lib/use-prices.ts
+++ packages/dapp/components/redeem/lib/use-prices.ts
@@ -12,7 +12,11 @@ const usePrices = (): [BigNumber | null, BigNumber | null, () => Promise<void>] 
   const [spotPrice, setSpotPrice] = useState<BigNumber | null>(null);
 
   async function refreshPrices() {
-    if (protocolContracts && provider) {
+    try {
+      if(!protocolContracts || !provider) {
+        return;
+      }
+
       const dollarTokenAddress = protocolContracts.managerFacet && await protocolContracts.managerFacet.dollarTokenAddress();
       const newTwapPrice = protocolContracts.twapOracleDollar3poolFacet && await protocolContracts.twapOracleDollar3poolFacet.consult(dollarTokenAddress);
       
@@ -21,6 +25,8 @@ const usePrices = (): [BigNumber | null, BigNumber | null, () => Promise<void>] 
       const newSpotPrice = await dollarMetapool["get_dy(int128,int128,uint256)"](0, 1, utils.parseEther("1"));
       setTwapPrice(newTwapPrice);
       setSpotPrice(newSpotPrice);
+    } catch (error) {
+      console.log("Error in refreshPrices: ", error)
     }
   }
 
