3865317d759c6a379aaeb31c3f7755ed136ea951
Doc Akan <fibrinlab@gmail.com>
2023-12-27 21:01:36 +0000

    feat: added strategy facet

---
+++ packages/contracts/src/dollar/facets/FuseRariAmoStrategyFacet.sol
@@ -0,0 +1,203 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {LibFuseRariAmoStrategy} from "../libraries/LibFuseRariAmoStrategy.sol";
+import {Modifiers} from "../libraries/LibAppStorage.sol";
+
+contract FuseRariAmoStrategyFacet is Modifiers {
+    function init(
+        address _dollar,
+        address[] memory _initialUnitrollers,
+        address[] memory _initialFusePools,
+        address _amoMinterAddress,
+        uint256 _globalDollarCollateralRatio
+    ) external {
+        LibFuseRariAmoStrategy.init(
+            _dollar,
+            _initialUnitrollers,
+            _initialFusePools,
+            _amoMinterAddress,
+            _globalDollarCollateralRatio
+        );
+    }
+
+    function showAllocations()
+        external
+        view
+        returns (uint256[3] memory allocations)
+    {
+        return LibFuseRariAmoStrategy.showAllocations();
+    }
+
+    function dollarBalances()
+        external
+        view
+        returns (uint256 dollarVal, uint256 collatVal)
+    {
+        return LibFuseRariAmoStrategy.dollarBalances();
+    }
+
+    function allPoolAddresses() external view returns (address[] memory) {
+        return LibFuseRariAmoStrategy.allPoolAddresses();
+    }
+
+    function allPoolsLength() external view returns (uint256) {
+        return LibFuseRariAmoStrategy.allPoolsLength();
+    }
+
+    function poolAddrToId(
+        address _poolAddress
+    ) external view returns (uint256) {
+        return LibFuseRariAmoStrategy.poolAddrToId(_poolAddress);
+    }
+
+    function dollarInPoolByPoolId(
+        uint256 _poolId
+    ) external view returns (uint256) {
+        return LibFuseRariAmoStrategy.dollarInPoolByPoolId(_poolId);
+    }
+
+    function dollarInPoolByPoolAddr(
+        address _poolAddress
+    ) external view returns (uint256) {
+        return LibFuseRariAmoStrategy.dollarInPoolByPoolAddr(_poolAddress);
+    }
+
+    // function mintedBalance() public view returns (int256) {
+    //     return amo_minter.frax_mint_balances(address(this));
+    // }
+
+    // // Backwards compatibility
+    // function accumulatedProfit() public view returns (int256) {
+    //     return int256(showAllocations()[2]) - mintedBalance();
+    // }
+
+    function allBorrowPoolAddresses() external view returns (address[] memory) {
+        return LibFuseRariAmoStrategy.allBorrowPoolAddresses();
+    }
+
+    function allBorrowPoolsLength() external view returns (uint256) {
+        return LibFuseRariAmoStrategy.allBorrowPoolsLength();
+    }
+
+    function borrowPoolAddrToId(
+        address _poolAddress
+    ) external view returns (uint256) {
+        return LibFuseRariAmoStrategy.borrowPoolAddrToId(_poolAddress);
+    }
+
+    function deptToPoolByPoolId(
+        uint256 _poolId
+    ) external view returns (uint256) {
+        return LibFuseRariAmoStrategy.deptToPoolByPoolId(_poolId);
+    }
+
+    function debtToPoolByPoolAddr(
+        address _poolAddress
+    ) external view returns (uint256) {
+        return LibFuseRariAmoStrategy.debtToPoolByPoolAddr(_poolAddress);
+    }
+
+    function assetDeptToPoolByPoolId(
+        uint256 _poolId
+    ) external view returns (uint256) {
+        return LibFuseRariAmoStrategy.assetDeptToPoolByPoolId(_poolId);
+    }
+
+    function assetDebtToPoolByPoolAddr(
+        address _poolAddress
+    ) external view returns (uint256) {
+        return LibFuseRariAmoStrategy.assetDebtToPoolByPoolAddr(_poolAddress);
+    }
+
+    /* ========== RESTRICTED FUNCTIONS ========== */
+
+    /* ---------------------------------------------------- */
+    /* ----------------------- Rari ----------------------- */
+    /* ---------------------------------------------------- */
+
+    /// @notice IRariComptroller can vary
+    function enterMarkets(
+        address _comptrollerAddress,
+        address _poolAddress
+    ) external {
+        LibFuseRariAmoStrategy.enterMarkets(_comptrollerAddress, _poolAddress);
+    }
+
+    /// @notice E18
+    function lendToPool(address _poolAddress, uint256 _lendAmount) external {
+        LibFuseRariAmoStrategy.lendToPool(_poolAddress, _lendAmount);
+    }
+
+    /// @notice E18
+    function redeemFromPool(
+        address _poolAddress,
+        uint256 _redeemAmount
+    ) external {
+        LibFuseRariAmoStrategy.redeemFromPool(_poolAddress, _redeemAmount);
+    }
+
+    /// @notice Auto compounds interest
+    function accrueInterest() external {
+        LibFuseRariAmoStrategy.accrueInterest();
+    }
+
+    /// @notice Borrow from pool
+    function borrowFromPool(
+        address _poolAddress,
+        uint256 _borrowAmount
+    ) external {
+        LibFuseRariAmoStrategy.borrowFromPool(_poolAddress, _borrowAmount);
+    }
+
+    /// @notice Repay borrowed asset to pool
+    function repayToPool(address _poolAddress, uint256 _repayAmount) external {
+        LibFuseRariAmoStrategy.repayToPool(_poolAddress, _repayAmount);
+    }
+
+    /// @notice Auto compounds interest for borrow pools
+    function accrueBorrowInterest() external {
+        LibFuseRariAmoStrategy.accrueBorrowInterest();
+    }
+
+    /* ========== Burns and givebacks ========== */
+
+    // Burn unneeded or excess DOLLAR. Goes through the minter
+    function burnDollar(uint256 _dollarAmount) external {
+        LibFuseRariAmoStrategy.burnDollar(_dollarAmount);
+    }
+
+    /* ========== OWNER / GOVERNANCE FUNCTIONS ONLY ========== */
+    // Only owner or timelock can call, to limit risk
+
+    // Add a fuse pool
+    function addFusePool(address _poolAddress) external {
+        LibFuseRariAmoStrategy.addFusePool(_poolAddress);
+    }
+
+    // Remove a fuse pool
+    function removeFusePool(address _poolAddress) external {
+        LibFuseRariAmoStrategy.removeFusePool(_poolAddress);
+    }
+
+    // Add a fuse pool for borrowing
+    function addBorrowFusePool(address _poolAddress) external {
+        LibFuseRariAmoStrategy.addBorrowFusePool(_poolAddress);
+    }
+
+    // Remove a borrow fuse pool
+    function removeBorrowFusePool(address _poolAddress) external {
+        LibFuseRariAmoStrategy.removeBorrowFusePool(_poolAddress);
+    }
+
+    function setAmoMinter(address _amoMinterAddress) external {
+        LibFuseRariAmoStrategy.setAmoMinter(_amoMinterAddress);
+    }
+
+    function recoverERC20(
+        address _tokenAddress,
+        uint256 _tokenAmount
+    ) external {
+        LibFuseRariAmoStrategy.recoverERC20(_tokenAddress, _tokenAmount);
+    }
+}
--- packages/contracts/src/dollar/libraries/LibFuseRariAmoStrategy.sol
+++ packages/contracts/src/dollar/libraries/LibFuseRariAmoStrategy.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import "@openzeppelin/contracts/utils/math/Math.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "../interfaces/IUbiquityDollarToken.sol";
 import "../interfaces/IRariComptroller.sol";
 import "../interfaces/IDollarAmoMinter.sol";
@@ -9,12 +10,20 @@ import "../interfaces/ICErc20Delegator.sol";
 import "./Constants.sol";
 
 library LibFuseRariAmoStrategy {
-    using Math for uint256;
+    using SafeERC20 for IERC20;
 
     bytes32 constant FUSERARIAMO_CONTROL_STORAGE_SLOT =
         bytes32(
             uint256(keccak256("ubiquity.contracts.fuserariamo.storage")) - 1
         );
+
+    /* ========== EVENTS ========== */
+
+    event FusePoolAdded(address token);
+    event FusePoolRemoved(address token);
+    event BorrowFusePoolAdded(address token);
+    event BorrowFusePoolRemoved(address token);
+    event Recovered(address token, uint256 amount);
 
     struct FuseRariAmoStrategyData {
         IUbiquityDollarToken DOLLAR;
@@ -23,7 +32,9 @@ library LibFuseRariAmoStrategy {
         address timelockAddress;
         address custodianAddress;
         address[] fusePoolsArray;
+        address[] fuseBorrowPoolsArray;
         mapping(address => bool) fusePools;
+        mapping(address => bool) fuseBorrowPools;
     }
 
     function fuseRariAmoStrategyStorage()
@@ -35,6 +46,25 @@ library LibFuseRariAmoStrategy {
         assembly {
             l.slot := slot
         }
+    }
+
+    modifier validPool(address _poolAddress) {
+        FuseRariAmoStrategyData
+            storage strategyStorage = fuseRariAmoStrategyStorage();
+
+        require(strategyStorage.fusePools[_poolAddress], "Invalid pool");
+        _;
+    }
+
+    modifier validBorrowPool(address _borrowPoolAddress) {
+        FuseRariAmoStrategyData
+            storage strategyStorage = fuseRariAmoStrategyStorage();
+
+        require(
+            strategyStorage.fuseBorrowPools[_borrowPoolAddress],
+            "Invalid borrow pool"
+        );
+        _;
     }
 
     function init(
@@ -148,5 +178,312 @@ library LibFuseRariAmoStrategy {
         );
         uint256 cTokenBalance = delegator.balanceOf(address(this));
         return cTokenBalance * (delegator.exchangeRateStored() / (1e18));
+    }
+
+    function dollarInPoolByPoolAddr(
+        address _poolAddress
+    ) internal view returns (uint256) {
+        uint256 poolId = poolAddrToId(_poolAddress);
+        return dollarInPoolByPoolId(poolId);
+    }
+
+    // function mintedBalance() public view returns (int256) {
+    //     return amo_minter.frax_mint_balances(address(this));
+    // }
+
+    // // Backwards compatibility
+    // function accumulatedProfit() public view returns (int256) {
+    //     return int256(showAllocations()[2]) - mintedBalance();
+    // }
+
+    function allBorrowPoolAddresses() internal view returns (address[] memory) {
+        FuseRariAmoStrategyData
+            storage strategyStorage = fuseRariAmoStrategyStorage();
+
+        return strategyStorage.fuseBorrowPoolsArray;
+    }
+
+    function allBorrowPoolsLength() internal view returns (uint256) {
+        FuseRariAmoStrategyData
+            storage strategyStorage = fuseRariAmoStrategyStorage();
+
+        return strategyStorage.fuseBorrowPoolsArray.length;
+    }
+
+    function borrowPoolAddrToId(
+        address _poolAddress
+    ) internal view returns (uint256) {
+        FuseRariAmoStrategyData
+            storage strategyStorage = fuseRariAmoStrategyStorage();
+
+        for (uint i = 0; i < strategyStorage.fuseBorrowPoolsArray.length; i++) {
+            if (strategyStorage.fuseBorrowPoolsArray[i] == _poolAddress) {
+                return i;
+            }
+        }
+        revert("Pool not found");
+    }
+
+    function deptToPoolByPoolId(
+        uint256 _poolId
+    ) internal view returns (uint256) {
+        FuseRariAmoStrategyData
+            storage strategyStorage = fuseRariAmoStrategyStorage();
+
+        ICErc20Delegator delegator = ICErc20Delegator(
+            strategyStorage.fuseBorrowPoolsArray[_poolId]
+        );
+        uint256 debt_bal = delegator.borrowBalanceStored(address(this));
+        return debt_bal * (delegator.exchangeRateStored() / 1e18);
+    }
+
+    function debtToPoolByPoolAddr(
+        address _poolAddress
+    ) internal view returns (uint256) {
+        uint256 poolId = borrowPoolAddrToId(_poolAddress);
+        return deptToPoolByPoolId(poolId);
+    }
+
+    function assetDeptToPoolByPoolId(
+        uint256 _poolId
+    ) internal view returns (uint256) {
+        FuseRariAmoStrategyData
+            storage strategyStorage = fuseRariAmoStrategyStorage();
+
+        ICErc20Delegator delegator = ICErc20Delegator(
+            strategyStorage.fuseBorrowPoolsArray[_poolId]
+        );
+        (, , uint debtBal, ) = delegator.getAccountSnapshot(address(this));
+        return debtBal;
+    }
+
+    function assetDebtToPoolByPoolAddr(
+        address _poolAddress
+    ) internal view returns (uint256) {
+        uint256 poolId = borrowPoolAddrToId(_poolAddress);
+        return assetDeptToPoolByPoolId(poolId);
+    }
+
+    /* ========== RESTRICTED FUNCTIONS ========== */
+
+    /* ---------------------------------------------------- */
+    /* ----------------------- Rari ----------------------- */
+    /* ---------------------------------------------------- */
+
+    /// @notice IRariComptroller can vary
+    function enterMarkets(
+        address _comptrollerAddress,
+        address _poolAddress
+    ) internal validPool(_poolAddress) {
+        address[] memory cTokens = new address[](1);
+        cTokens[0] = _poolAddress;
+        IRariComptroller(_comptrollerAddress).enterMarkets(cTokens);
+    }
+
+    /// @notice E18
+    function lendToPool(
+        address _poolAddress,
+        uint256 _lendAmount
+    ) internal validPool(_poolAddress) {
+        FuseRariAmoStrategyData
+            storage strategyStorage = fuseRariAmoStrategyStorage();
+
+        uint256 poolId = poolAddrToId(_poolAddress);
+        strategyStorage.DOLLAR.approve(_poolAddress, _lendAmount);
+        ICErc20Delegator(strategyStorage.fusePoolsArray[poolId]).mint(
+            _lendAmount
+        );
+    }
+
+    /// @notice E18
+    function redeemFromPool(
+        address _poolAddress,
+        uint256 _redeemAmount
+    ) internal validPool(_poolAddress) {
+        FuseRariAmoStrategyData
+            storage strategyStorage = fuseRariAmoStrategyStorage();
+
+        uint256 poolId = poolAddrToId(_poolAddress);
+        ICErc20Delegator(strategyStorage.fusePoolsArray[poolId])
+            .redeemUnderlying(_redeemAmount);
+    }
+
+    /// @notice Auto compounds interest
+    function accrueInterest() internal {
+        FuseRariAmoStrategyData
+            storage strategyStorage = fuseRariAmoStrategyStorage();
+
+        for (uint i = 0; i < strategyStorage.fusePoolsArray.length; i++) {
+            // Make sure the pool is enabled first
+            address pool_address = strategyStorage.fusePoolsArray[i];
+            if (strategyStorage.fusePools[pool_address]) {
+                ICErc20Delegator(strategyStorage.fusePoolsArray[i])
+                    .accrueInterest();
+            }
+        }
+    }
+
+    /// @notice Borrow from pool
+    function borrowFromPool(
+        address _poolAddress,
+        uint256 _borrowAmount
+    ) internal validBorrowPool(_poolAddress) {
+        ICErc20Delegator(_poolAddress).borrow(_borrowAmount);
+    }
+
+    /// @notice Repay borrowed asset to pool
+    function repayToPool(
+        address _poolAddress,
+        uint256 _repayAmount
+    ) internal validBorrowPool(_poolAddress) {
+        ICErc20Delegator delegator = ICErc20Delegator(_poolAddress);
+        ERC20(delegator.underlying()).approve(_poolAddress, _repayAmount);
+        delegator.repayBorrow(_repayAmount);
+    }
+
+    /// @notice Auto compounds interest for borrow pools
+    function accrueBorrowInterest() internal {
+        FuseRariAmoStrategyData
+            storage strategyStorage = fuseRariAmoStrategyStorage();
+
+        for (uint i = 0; i < strategyStorage.fuseBorrowPoolsArray.length; i++) {
+            // Make sure the pool is enabled first
+            address poolAddress = strategyStorage.fuseBorrowPoolsArray[i];
+            if (strategyStorage.fuseBorrowPools[poolAddress]) {
+                ICErc20Delegator(strategyStorage.fuseBorrowPoolsArray[i])
+                    .accrueInterest();
+            }
+        }
+    }
+
+    /* ========== Burns and givebacks ========== */
+
+    // Burn unneeded or excess DOLLAR. Goes through the minter
+    function burnDollar(uint256 _dollarAmount) internal {
+        FuseRariAmoStrategyData
+            storage strategyStorage = fuseRariAmoStrategyStorage();
+
+        strategyStorage.DOLLAR.approve(
+            address(strategyStorage.amoMinter),
+            _dollarAmount
+        );
+        strategyStorage.amoMinter.burnDollarFromAmo(_dollarAmount);
+    }
+
+    /* ========== OWNER / GOVERNANCE FUNCTIONS ONLY ========== */
+    // Only owner or timelock can call, to limit risk
+
+    // Add a fuse pool
+    function addFusePool(address _poolAddress) internal {
+        FuseRariAmoStrategyData
+            storage strategyStorage = fuseRariAmoStrategyStorage();
+
+        require(_poolAddress != address(0), "Zero address detected");
+
+        require(
+            strategyStorage.fusePools[_poolAddress] == false,
+            "Address already exists"
+        );
+        strategyStorage.fusePools[_poolAddress] = true;
+        strategyStorage.fusePoolsArray.push(_poolAddress);
+
+        emit FusePoolAdded(_poolAddress);
+    }
+
+    // Remove a fuse pool
+    function removeFusePool(address _poolAddress) internal {
+        FuseRariAmoStrategyData
+            storage strategyStorage = fuseRariAmoStrategyStorage();
+
+        require(_poolAddress != address(0), "Zero address detected");
+        require(
+            strategyStorage.fusePools[_poolAddress] == true,
+            "Address nonexistent"
+        );
+
+        // Delete from the mapping
+        delete strategyStorage.fusePools[_poolAddress];
+
+        // 'Delete' from the array by setting the address to 0x0
+        for (uint i = 0; i < strategyStorage.fusePoolsArray.length; i++) {
+            if (strategyStorage.fusePoolsArray[i] == _poolAddress) {
+                strategyStorage.fusePoolsArray[i] = address(0); // This will leave a null in the array and keep the indices the same
+                break;
+            }
+        }
+
+        emit FusePoolRemoved(_poolAddress);
+    }
+
+    // Add a fuse pool for borrowing
+    function addBorrowFusePool(address _poolAddress) internal {
+        FuseRariAmoStrategyData
+            storage strategyStorage = fuseRariAmoStrategyStorage();
+
+        require(_poolAddress != address(0), "Zero address detected");
+        require(
+            strategyStorage.fuseBorrowPools[_poolAddress] == false,
+            "Address already exists"
+        );
+        strategyStorage.fuseBorrowPools[_poolAddress] = true;
+        strategyStorage.fuseBorrowPoolsArray.push(_poolAddress);
+
+        emit BorrowFusePoolAdded(_poolAddress);
+    }
+
+    // Remove a borrow fuse pool
+    function removeBorrowFusePool(address _poolAddress) internal {
+        FuseRariAmoStrategyData
+            storage strategyStorage = fuseRariAmoStrategyStorage();
+
+        require(_poolAddress != address(0), "Zero address detected");
+        require(
+            strategyStorage.fuseBorrowPools[_poolAddress] == true,
+            "Address nonexistent"
+        );
+
+        // Delete from the mapping
+        delete strategyStorage.fuseBorrowPools[_poolAddress];
+
+        // 'Delete' from the array by setting the address to 0x0
+        for (uint i = 0; i < strategyStorage.fuseBorrowPoolsArray.length; i++) {
+            if (strategyStorage.fuseBorrowPoolsArray[i] == _poolAddress) {
+                strategyStorage.fuseBorrowPoolsArray[i] = address(0); // This will leave a null in the array and keep the indices the same
+                break;
+            }
+        }
+
+        emit BorrowFusePoolRemoved(_poolAddress);
+    }
+
+    function setAmoMinter(address _amoMinterAddress) internal {
+        FuseRariAmoStrategyData
+            storage strategyStorage = fuseRariAmoStrategyStorage();
+
+        strategyStorage.amoMinter = IDollarAmoMinter(_amoMinterAddress);
+
+        // Get the custodian and timelock addresses from the minter
+        strategyStorage.custodianAddress = strategyStorage
+            .amoMinter
+            .custodianAddress();
+        strategyStorage.timelockAddress = strategyStorage
+            .amoMinter
+            .timelockAddress();
+
+        // Make sure the new addresses are not address(0)
+        require(
+            strategyStorage.custodianAddress != address(0) &&
+                strategyStorage.timelockAddress != address(0),
+            "Invalid custodian or timelock"
+        );
+    }
+
+    function recoverERC20(
+        address _tokenAddress,
+        uint256 _tokenAmount
+    ) internal {
+        IERC20(_tokenAddress).safeTransfer(msg.sender, _tokenAmount);
+
+        emit Recovered(_tokenAddress, _tokenAmount);
     }
 }
