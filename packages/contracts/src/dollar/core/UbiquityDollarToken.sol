6bde5ab08595189d9c81d86b13f4627d393ac3d2
molecula451 <web3samuraidev@gmail.com>
2024-01-18 17:38:23 -0400

    fix: deprecate incentive on dollar token

--- packages/contracts/src/dollar/core/UbiquityDollarToken.sol
+++ packages/contracts/src/dollar/core/UbiquityDollarToken.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.19;
 
 import {ERC20Ubiquity} from "./ERC20Ubiquity.sol";
 import {IERC20Ubiquity} from "../../dollar/interfaces/IERC20Ubiquity.sol";
-import {IIncentive} from "../../dollar/interfaces/IIncentive.sol";
 
 import "../libraries/Constants.sol";
 
@@ -11,18 +10,6 @@ import "../libraries/Constants.sol";
  * @notice Ubiquity Dollar token contract
  */
 contract UbiquityDollarToken is ERC20Ubiquity {
-    /**
-     * @notice Mapping of account and incentive contract address
-     * @dev Address is 0 if there is no incentive contract for the account
-     */
-    mapping(address => address) public incentiveContract;
-
-    /// @notice Emitted on setting an incentive contract for an account
-    event IncentiveContractUpdate(
-        address indexed _incentivized,
-        address indexed _incentiveContract
-    );
-
     /// @notice Ensures initialize cannot be called on the implementation contract
     constructor() {
         _disableInitializers();
@@ -56,86 +43,6 @@ contract UbiquityDollarToken is ERC20Ubiquity {
     }
 
     /**
-     * @notice Sets `incentive` contract for `account`
-     * @notice Incentive contracts are applied on Dollar transfers:
-     * - EOA => contract
-     * - contract => EOA
-     * - contract => contract
-     * - any transfer global incentive
-     * @param account Account to incentivize
-     * @param incentive Incentive contract address
-     */
-    function setIncentiveContract(address account, address incentive) external {
-        require(
-            accessControl.hasRole(GOVERNANCE_TOKEN_MANAGER_ROLE, _msgSender()),
-            "Dollar: must have admin role"
-        );
-
-        incentiveContract[account] = incentive;
-        emit IncentiveContractUpdate(account, incentive);
-    }
-
-    /**
-     * @notice Applies incentives on Dollar transfers
-     * @param sender Sender address
-     * @param recipient Recipient address
-     * @param amount Dollar token transfer amount
-     */
-    function _checkAndApplyIncentives(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) internal {
-        // incentive on sender
-        address senderIncentive = incentiveContract[sender];
-        if (senderIncentive != address(0)) {
-            IIncentive(senderIncentive).incentivize(
-                sender,
-                recipient,
-                _msgSender(),
-                amount
-            );
-        }
-
-        // incentive on recipient
-        address recipientIncentive = incentiveContract[recipient];
-        if (recipientIncentive != address(0)) {
-            IIncentive(recipientIncentive).incentivize(
-                sender,
-                recipient,
-                _msgSender(),
-                amount
-            );
-        }
-
-        // incentive on operator
-        address operatorIncentive = incentiveContract[_msgSender()];
-        if (
-            _msgSender() != sender &&
-            _msgSender() != recipient &&
-            operatorIncentive != address(0)
-        ) {
-            IIncentive(operatorIncentive).incentivize(
-                sender,
-                recipient,
-                _msgSender(),
-                amount
-            );
-        }
-
-        // all incentive, if active applies to every transfer
-        address allIncentive = incentiveContract[address(0)];
-        if (allIncentive != address(0)) {
-            IIncentive(allIncentive).incentivize(
-                sender,
-                recipient,
-                _msgSender(),
-                amount
-            );
-        }
-    }
-
-    /**
      * @notice Moves `amount` of tokens from `from` to `to` and applies incentives.
      *
      * This internal function is equivalent to `transfer`, and can be used to
@@ -155,7 +62,6 @@ contract UbiquityDollarToken is ERC20Ubiquity {
         uint256 amount
     ) internal override {
         super._transfer(sender, recipient, amount);
-        _checkAndApplyIncentives(sender, recipient, amount);
     }
 
     /**
