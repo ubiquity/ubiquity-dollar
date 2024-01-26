6bb7f4dd1ab4b13b06bdd7fb139b97162987c793
molecula451 <web3samuraidev@gmail.com>
2024-01-26 12:40:39 -0400

    chore: update config and wallet connect

--- packages/dapp/ubq.config.ts
+++ packages/dapp/ubq.config.ts
@@ -4,8 +4,6 @@ export type UbiquityConfig = {
 
 const ubqConfig = {
   // This is ours WalletConnect's default project ID.
-  // It's recommended to store it in an env variable:
-  // .env.local for local testing, and in the Vercel/system env config for live apps.
   walletConnectProjectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "ef004b660177d5660241cdb882fcbf84",
 } as const satisfies UbiquityConfig;
 
