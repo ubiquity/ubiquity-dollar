4ce85ac0add8e160ed0e2abb16c8c7183c7b9997
rndquu <rndquu@gmail.com>
2024-03-25 11:43:31 +0300

    refactor(migration): deploy collateral in testnet

--- packages/contracts/src/dollar/mocks/MockERC20.sol
+++ packages/contracts/src/dollar/mocks/MockERC20.sol
@@ -4,11 +4,15 @@ pragma solidity 0.8.19;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockERC20 is ERC20 {
+    uint8 internal __decimals;
+
     constructor(
         string memory _name,
         string memory _symbol,
         uint8 _decimals
-    ) ERC20(_name, _symbol) {}
+    ) ERC20(_name, _symbol) {
+        __decimals = _decimals;
+    }
 
     function mint(address to, uint256 value) public virtual {
         _mint(to, value);
@@ -16,5 +20,9 @@ contract MockERC20 is ERC20 {
 
     function burn(address from, uint256 value) public virtual {
         _burn(from, value);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return __decimals;
     }
 }
