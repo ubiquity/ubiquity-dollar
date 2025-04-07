66ef894c9ece0b703d0f79cdc46750ac1a140df3
rndquu <rndquu@gmail.com>
2025-04-07 13:15:38 +0300

    chore: add development migration

--- packages/contracts/migrations/development/deploy.sh
+++ packages/contracts/migrations/development/deploy.sh
@@ -5,3 +5,6 @@ source .env
 
 # Deploy001_Diamond_Dollar_Governance (deploys Diamond, Dollar and Governance related contracts)
 forge script migrations/development/Deploy001_Diamond_Dollar_Governance.s.sol:Deploy001_Diamond_Dollar_Governance --rpc-url $RPC_URL --broadcast -vvvv
+
+# Deploy002_Staking (deploys `StakingFacet`)
+forge script migrations/development/Deploy002_Staking.s.sol:Deploy002_Staking --rpc-url $RPC_URL --broadcast -vvvv
