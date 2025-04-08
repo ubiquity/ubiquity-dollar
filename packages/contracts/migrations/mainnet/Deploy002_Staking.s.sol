e110534b61db0066641c272a25f8c80f01cced62
rndquu <rndquu@gmail.com>
2025-04-08 12:52:56 +0300

    chore: add production staking migration

--- packages/contracts/migrations/mainnet/Deploy002_Staking.s.sol
+++ packages/contracts/migrations/mainnet/Deploy002_Staking.s.sol
@@ -3,64 +3,60 @@ pragma solidity 0.8.19;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Script} from "forge-std/Script.sol";
-import {UbiquityAlgorithmicDollarManager} from "../../src/deprecated/UbiquityAlgorithmicDollarManager.sol";
-import {UbiquityGovernance} from "../../src/deprecated/UbiquityGovernance.sol";
 import {DiamondCutFacet} from "../../src/dollar/facets/DiamondCutFacet.sol";
 import {StakingFacet} from "../../src/dollar/facets/StakingFacet.sol";
-import {MockERC20} from "../../src/dollar/mocks/MockERC20.sol";
 import {DiamondTestHelper} from "../../test/helpers/DiamondTestHelper.sol";
 
 /**
  * @notice Migration contract
  * @dev Deploys `StakingFacet`
+ * NOTICE: in order to QA this migration locally use this shell script:
+ * ```
+ * #!/bin/bash
+ *
+ * # load env variables
+ * source .env
+ *
+ * UBQ_ETH_ADDRESS=0xefC0e701A824943b469a694aC564Aa1efF7Ab7dd;
+ *
+ * # pretend we're `ubq.eth` address
+ * cast rpc anvil_impersonateAccount $UBQ_ETH_ADDRESS;
+ *
+ * # Deploy002_Staking (deploys `StakingFacet`)
+ * forge script migrations/mainnet/Deploy002_Staking.s.sol:Deploy002_Staking --rpc-url $RPC_URL --broadcast -vvvv --unlocked
+ * 
+ * # stop pretending we're `ubq.eth` address
+ * cast rpc anvil_stopImpersonatingAccount $UBQ_ETH_ADDRESS;
+ * ```
  */
 contract Deploy002_Staking is Script, DiamondTestHelper {
-    UbiquityAlgorithmicDollarManager dollarManager;
-    UbiquityGovernance rewardToken;
-    MockERC20 stakeToken;
-
     // env variables
-    uint256 adminPrivateKey;
     uint256 ownerPrivateKey;
-    address diamond;
 
-    // owner and admin addresses derived from private keys store in `.env` file
-    address adminAddress;
+    // owner address derived from private key stored in `.env` file
     address ownerAddress;
+
+    address diamond = 0xED3084c98148e2528DaDCB53C56352e549C488fA;
+    address ubiquityDeployerAddress = 0xefC0e701A824943b469a694aC564Aa1efF7Ab7dd;
+    address ubqToken = 0x4e38D89362f7e5db0096CE44ebD021c3962aA9a0;
+    address lusdUusdLpToken = 0xcC68509F9cA0E1ed119EAC7c468EC1b1C42f384F;
 
     function run() public {
         // read env variables
-        diamond = vm.envAddress("DIAMOND_ADDRESS"); // env variable set in `deploy.sh`
-        adminPrivateKey = vm.envUint("ADMIN_PRIVATE_KEY");
         ownerPrivateKey = vm.envUint("OWNER_PRIVATE_KEY");
-
-        adminAddress = vm.addr(adminPrivateKey);
         ownerAddress = vm.addr(ownerPrivateKey);
 
-        //====================================
-        // Deploy reward and staking tokens
-        //====================================
-
-        // start sending owner transactions
-        vm.startBroadcast(ownerPrivateKey);
-
-        dollarManager = new UbiquityAlgorithmicDollarManager(ownerAddress);
-        rewardToken = new UbiquityGovernance(address(dollarManager));
-        stakeToken = new MockERC20("STK", "STK", 18);
-
-        // owner grants diamond the "UBQ_MINTER_ROLE" 
-        // NOTICE: in production environment the diamond contract already has the "UBQ_MINTER_ROLE" role
-        dollarManager.grantRole(keccak256("UBQ_MINTER_ROLE"), address(diamond));
-
-        // stop sending owner transactions
-        vm.stopBroadcast();
+        // Start sending owner transactions
+        // If owner is `ubq.eth` then send real transactions, otherwise simulate calls from `ubq.eth`
+        if (ownerAddress == ubiquityDeployerAddress) {
+            vm.startBroadcast(ownerPrivateKey);
+        } else {
+            vm.startBroadcast(ubiquityDeployerAddress);
+        }
 
         //===================================================
         // Deploy `StakingFacet` and add it to the diamond
         //===================================================
-
-        // start sending owner transactions
-        vm.startBroadcast(ownerPrivateKey);
 
         // prepare staking facet selectors
         bytes4[] memory selectorsOfStakingFacet = getSelectorsFromAbi("/out/StakingFacet.sol/StakingFacet.json");
@@ -82,28 +78,24 @@ contract Deploy002_Staking is Script, DiamondTestHelper {
         DiamondCutFacet diamondCutFacet = DiamondCutFacet(diamond);
         diamondCutFacet.diamondCut(cuts, address(0), "");
 
-        // stop sending owner transactions
-        vm.stopBroadcast();
-
         //====================
         // Add staking pool
         //====================
 
-        // start sending admin transactions
-        vm.startBroadcast(adminPrivateKey);
-
+        // NOTICE: adding a new staking pool requires `admin` role (not `owner`) but since `admin`
+        // and `owner` are the same addresses (`ubq.eth`) then it's safe to use the `owner` role
         StakingFacet stakingFacet = StakingFacet(diamond);
         stakingFacet.setGovernancePerBlock(1 ether); // 1 reward token minted per block
         stakingFacet.setGovernanceTreasuryDivider(5); // `100 / 5 = 20%` extra reward tokens minted for treasury
-        stakingFacet.setStakingRewardToken(address(rewardToken)); // reward token address
+        stakingFacet.setStakingRewardToken(ubqToken); // reward token address
         stakingFacet.setStakingStartBlock(block.number); // activate staking from current block
         stakingFacet.createStakingPool( // add a new staking pool
             100, // allocation points
-            IERC20(stakeToken), // staking token
+            IERC20(lusdUusdLpToken), // staking token
             true // whether to update all pools
         );
 
-        // stop sending admin transactions
+        // Stop sending owner transactions
         vm.stopBroadcast();
     }
 }
